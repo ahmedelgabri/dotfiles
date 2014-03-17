bd8a27bfe0580d812921e5d9a5b4968b2e57a05d
Ahmed El Gabri <ahmed@gabri.me>
2014-03-17 22:02:02 +0400

    :sleepy:

--- localhost/localhost.sh
+++ localhost/localhost.sh
@@ -1,6 +1,6 @@
 sudo rm /etc/apache2/extra/httpd-vhosts.conf
 sudo mkdir /etc/resolver
-sudo ln -s $HOME/.dotfiles/localhost/httpd-vhosts.conf /etc/apache2/extra/httpd-vhosts.conf
-ln -s $HOME/.dotfiles/localhost/dnsmasq.conf /usr/local/etc/dnsmasq.conf
-sudo ln -s $HOME/.dotfiles/localhost/gabri /etc/resolver/gabri
-sudo ln -s $HOME/.dotfiles/localhost/trunk /etc/resolver/trunk
+sudo ln -s $HOME/.dotfiles/localhost/httpd-vhosts.local /etc/apache2/extra/httpd-vhosts.conf
+ln -s $HOME/.dotfiles/localhost/dnsmasq.local /usr/local/etc/dnsmasq.conf
+sudo ln -s $HOME/.dotfiles/localhost/gabri.local /etc/resolver/gabri
+sudo ln -s $HOME/.dotfiles/localhost/trunk.local /etc/resolver/trunk
