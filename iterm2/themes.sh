76c6e671a2656e302c714c6fc08113fa3cdef6b3
Ahmed El Gabri <ahmed@gabri.me>
2015-06-18 20:30:42 +0200

    Updates :boom: too lazy to write

---
+++ iterm2/themes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -O $HOME/.dotfiles/iterm2/gruvbox-light.itermcolors https://raw.githubusercontent.com/morhetz/gruvbox-generalized/master/iterm2/gruvbox-light.itermcolors
+wget -O $HOME/.dotfiles/iterm2/gruvbox-dark.itermcolors https://raw.githubusercontent.com/morhetz/gruvbox-generalized/master/iterm2/gruvbox-dark.itermcolors
+wget -O $HOME/.dotfiles/iterm2/base16-eighties.dark.256.itermcolors https://raw.githubusercontent.com/chriskempson/base16-iterm2/master/base16-eighties.dark.256.itermcolors
+wget -O $HOME/.dotfiles/iterm2/base16-eighties.light.256.itermcolors https://raw.githubusercontent.com/chriskempson/base16-iterm2/master/base16-eighties.light.256.itermcolors
+wget -O $HOME/.dotfiles/iterm2/base16-ocean.dark.256.itermcolors https://raw.githubusercontent.com/chriskempson/base16-iterm2/master/base16-ocean.dark.256.itermcolors
+wget -O $HOME/.dotfiles/iterm2/base16-ocean.light.256.itermcolors https://raw.githubusercontent.com/chriskempson/base16-iterm2/master/base16-ocean.light.256.itermcolors
---
+++ weechat/python/autoload/emojis.py
@@ -0,0 +1,1 @@
+/Users/Gabri/.weechat/python/emojis.py
