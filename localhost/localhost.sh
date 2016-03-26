9a38ed95ce1b94488ff7ed9540eece820c1c4be3
Ahmed El Gabri <ahmed@gabri.me>
2016-03-26 14:33:29 +0100

    Remove localhost

--- localhost/localhost.sh
+++
@@ -1,7 +0,0 @@
-sudo rm /etc/apache2/extra/httpd-vhosts.conf
-sudo mkdir /etc/resolver
-sudo ln -s $HOME/.dotfiles/localhost/httpd-vhosts.local /etc/apache2/extra/httpd-vhosts.conf
-ln -s $HOME/.dotfiles/localhost/dnsmasq.local /usr/local/etc/dnsmasq.conf
-sudo ln -s $HOME/.dotfiles/localhost/gabri.local /etc/resolver/gabri
-sudo ln -s $HOME/.dotfiles/localhost/trunk.local /etc/resolver/trunk
-sudo ln -s $HOME/.dotfiles/localhost/dev.local /etc/resolver/dev
