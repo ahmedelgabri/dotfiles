c8bb9115809b2995d9b6b87bb3dd03e810bf7019
Ahmed El Gabri <ahmed@gabri.me>
2016-06-17 17:32:15 +0200

    Update JS snippets

--- vim/.vim/pythonx/helpers.py
+++ vim/.vim/pythonx/helpers.py
@@ -1,4 +1,38 @@
+# https://github.com/lencioni/dotfiles/blob/eddc37169fd36648791a395a47e2ead3fcadcb87/.vim/pythonx/snippet_helpers.py
+
+import os
 import re
+
+
+def pascal_case_basename(basename):
+    return ''.join(x[0].upper() + x[1:] for x in basename.split('_'))
+
+
+def dasherize_basename(basename):
+    return '-'.join(filter(None, _convert_camel_case(basename).split('_')))
+
+
+# http://stackoverflow.com/a/1176023/18986
+def _convert_camel_case(string):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', string)
+    return re.sub('([a-z0-9])([A-Z])', r'\1-\2', s1).lower()
+
+
+def _clean_basename(basename):
+    return re.sub('(_spec|-test)$', '', basename or 'ModuleName')
+
+# If path ends in `index.jsx`, this function will return the PascalCased
+# directory name. Otherwise, it returns the PascalCased filename. This allows
+# me to use my snippets with modules that are like `/path/to/module_name.jsx`
+# and modules that are like `/path/to/ModuleName/index.jsx`.
+def path_to_component_name(path, case_fn):
+    dirname, filename = os.path.split(path)
+    basename = os.path.splitext(filename)[0]
+    if basename in ['index', 'index-test']:
+        # Pop the last directory name off the dirname
+        return case_fn(_clean_basename(os.path.basename(dirname)))
+    else:
+        return case_fn(_clean_basename(basename))
 
 def complete(t, opts):
   if t:
@@ -8,15 +42,13 @@ def complete(t, opts):
   return '(' + '|'.join(opts) + ')'
 
 
-def completeVar(t):
-  return complete(t, ['var','let','const'])
-
 def formatTag(argument):
   return " * @param {{}} {0}\n".format(argument)
 
 def jsDoc(tabStop):
   # Currently Ultisnips does not support dynamic tabstops, so we cannot add
   # tabstops to the datatype for these param tags until that feature is added.
+  # arguments = tabStop.split(',') if tabStop[0] != '{' else tabStop[1:-1].split(',')
   arguments = tabStop.split(',')
   arguments = [argument.strip() for argument in arguments if argument]
 
@@ -38,8 +70,22 @@ def formatVariableName(path):
   if lastPart == 'lodash':
     return '_'
   elif lastPart == 'react':
-    return 'React'
+    return 'React, { Component, PropTypes }'
+  elif lastPart == 'react-dom':
+    return 'ReactDOM';
   elif lastPart == 'jquery':
     return '$'
   else:
     return re.sub(r'[_\-]', '', lastPart)
+
+
+# https://github.com/wincent/wincent/blob/9b938b4d879a2/roles/dotfiles/files/.vim/ultisnips/javascript.snippets#L38-L46
+def react_es6(current_buffer):
+  """
+  Checks the current buffer to see whether React ES6 syntax should be used.
+  """
+  buffer = current_buffer
+  for line in buffer:
+    if re.search('React.createClass', line):
+      return False;
+    return True;
