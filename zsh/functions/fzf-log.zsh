97a3d73d168e0b6376f76efe99f7e212fe689e82
Ahmed El Gabri <ahmed@gabri.me>
2016-08-21 19:27:46 +0200

    Better format for fzf-log script

--- zsh/functions/fzf-log.zsh
+++ zsh/functions/fzf-log.zsh
@@ -1,6 +1,6 @@
 # git commit browser. needs fzf
 log() {
-  git lg "$@" |
+  git log --pretty=format:"%h - (%cr) %s - %an %d" --date=short "$@" |
   fzf --no-sort --reverse --tiebreak=index --toggle-sort=\` \
       --bind "ctrl-m:execute:
                 echo '{}' | grep -o '[a-f0-9]\{7\}' | head -1 |
