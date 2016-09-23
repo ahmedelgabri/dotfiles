eb31b6253c12c1853f3c36d54a80bd365aea4c46
Ahmed El Gabri <ahmed@gabri.me>
2016-09-23 21:58:09 +0200

    uncomment fco

--- zsh/functions/fzf-z.zsh
+++ zsh/functions/fzf-z.zsh
@@ -13,16 +13,16 @@
 #   cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf -q $_last_z_args)"
 # }
 
-# fco() {
-#   local tags branches target
-#   tags=$(
-#     git tag | awk '{print "\x1b[31;1mtag\x1b[m\t" $1}') || return
-#   branches=$(
-#     git branch --all | grep -v HEAD             |
-#     sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
-#     sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
-#   target=$(
-#     (echo "$tags"; echo "$branches") |
-#     fzf-tmux -- --no-hscroll --ansi +m -d "\t" -n 2) || return
-#   git checkout $(echo "$target" | awk '{print $2}')
-# }
+fco() {
+  local tags branches target
+  tags=$(
+    git tag | awk '{print "\x1b[31;1mtag\x1b[m\t" $1}') || return
+  branches=$(
+    git branch --all | grep -v HEAD             |
+    sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
+    sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
+  target=$(
+    (echo "$tags"; echo "$branches") |
+    fzf-tmux -- --no-hscroll --ansi +m -d "\t" -n 2) || return
+  git checkout $(echo "$target" | awk '{print $2}')
+}
