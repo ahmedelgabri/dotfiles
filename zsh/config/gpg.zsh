6710e912ba8fc43ebad304c8f6c1fb00514cbccc
Ahmed El Gabri <ahmed@gabri.me>
2017-04-08 11:56:05 +0200

    gpg: remove old options

--- zsh/config/gpg.zsh
+++ zsh/config/gpg.zsh
@@ -8,5 +8,6 @@ if [ -f ~/.gnupg/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
     source ~/.gnupg/.gpg-agent-info
     export GPG_AGENT_INFO
 else
-    eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
+    # eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
+    eval $(gpg-agent --daemon)
 fi
