cce3181bc3b5a7693247943ad48ba8614e00c2b1
Ahmed El Gabri <ahmed@gabri.me>
2017-08-20 16:03:27 +0200

    zsh: improve exa options & match it with ls

--- zsh/config/functions/l.zsh
+++ zsh/config/functions/l.zsh
@@ -5,12 +5,13 @@ l(){
     if [[ "$1" == '--help' ]]; then
       exa --help
     else
-      exa --all --long --group --header --links --color-scale --group-directories-first "$@"
+      # --all --long --group --header --links --classify --color-scale --group-directories-first --sort=type
+      exa -alghHF --color-scale --group-directories-first --sort=type "$@"
     fi
   else
     # https://github.com/paulirish/dotfiles/blob/7c46f8c25015c2632894dbe5fea20014ab37fd89/.functions#L14-L25
     # List all files, long format, colorized, permissions in octal
-    ls -AlhF --color=always "$@" | awk '
+    ls -AlhF --group-directories-first --color=always "$@" | awk '
       {
         k=0;
         for (i=0;i<=8;i++)
