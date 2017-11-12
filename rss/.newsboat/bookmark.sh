66734eb76d805f13ea50df0dff127cd23ced3e5d
Ahmed El Gabri <ahmed@gabri.me>
2017-11-12 18:18:32 +0100

    RSS: major update to newsboat config

---
+++ rss/.newsboat/bookmark.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+[ "$#" -eq 0 ] && exit 1
+if [ -n $(command -v curl) ]; then
+  url=$(curl -sIL -o /dev/null -w '%{url_effective}' "$1")
+else
+  url="$1"
+fi
+url=$(echo "${url}" | perl -p -e 's/(\?|\&)?utm_[a-z]+=[^\&]+//g;' -e 's/(#|\&)?utm_[a-z]+=[^\&]+//g;')
+title="$2"
+description="$3"
+
+grep -q "${url}\t${title}\t${description}" ~/.newsbeuter/bookmarks.txt || echo "${url}\t${title}\t${description}" >> ~/.newsbeuter/bookmarks.txt
