136667e7e954e68d2fcb769748e4045efe02198d
Ahmed El Gabri <ahmed@gabri.me>
2017-12-02 19:24:23 +0100

    script: cleanup python packages & add pipenv

--- script/python-packages.sh
+++ script/python-packages.sh
@@ -3,20 +3,15 @@
 PACKAGES=(
 "--upgrade setuptools"
 "--upgrade pip"
-# "numpy"
-# "pygments"
-# "virtualenv"
-# "virtualenvwrapper"
 # "markdown"
 "neovim"
 "vim-vint"
 "websocket-client"
+"pipenv"
 )
 
-# mkdir ~/.venv 
-
 for package in "${PACKAGES[@]}"; do
-  pip3 install "$package" && pip2 install "$package"
+  pip3 install --user "$package" && pip2 install --user "$package"
 done
 
 unset -v PACKAGES
