6731944952739958b7962c6aa76ba4734eb07af1
Ahmed El Gabri <ahmed@gabri.me>
2018-01-19 21:18:12 +0100

    zsh: Cleanup alias

--- zsh/zshrc.d/config/aliases.zsh
+++ zsh/zshrc.d/config/aliases.zsh
@@ -37,7 +37,7 @@ alias du='du -kh'
 alias ev="e ~/.vimrc"
 alias "?"="pwd"
 alias c="clear "
-alias KABOOM="(yarn global upgrade --latest && brew update && brew upgrade && brew cleanup -s --force && brew prune && brew cask cleanup && brew doctor); source ~/.zshrc"
+alias KABOOM="yarn global upgrade --latest && brew update && brew upgrade && brew cleanup -s --force && brew prune && brew cask cleanup && brew doctor"
 alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes;sudo rm -rfv ~/.Trash"
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
