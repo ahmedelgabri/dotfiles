2397c8de37abe5459e8fc50a3448b4234e653709
Ahmed El Gabri <ahmed@gabri.me>
2018-01-23 23:12:55 +0100

    gpg: update config & remove comment

--- zsh/zshrc.d/config/gpg.zsh
+++ zsh/zshrc.d/config/gpg.zsh
@@ -5,9 +5,8 @@
 
 # Add the following to your shell init to set up gpg-agent automatically for every shell
 if [ -f ~/.gnupg/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
-    source ~/.gnupg/.gpg-agent-info
-    export GPG_AGENT_INFO
+  source ~/.gnupg/.gpg-agent-info
+  export GPG_AGENT_INFO
 else
-    # eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
-    eval $(gpg-agent --daemon)
+  eval $(gpg-agent --daemon)
 fi
