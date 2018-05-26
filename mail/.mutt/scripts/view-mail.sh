ee3b9b2b86c24261fb9f9ae68d5e53ea9e0e4644
Ahmed El Gabri <ahmed@gabri.me>
2018-05-26 14:18:38 +0200

    mail: fix mailcap & viewing plain text mails in nvim

--- mail/.mutt/scripts/view-mail.sh
+++ mail/.mutt/scripts/view-mail.sh
@@ -8,13 +8,12 @@ ENCODING="${2:utf-8}"
 DIR=$(mktemp -d)
 cp "$TMPFILE" "$DIR/preview"
 TMPFILE="$DIR/preview"
+PANE=$(tmux display -pt "$TMUX_PANE" '#S:#I.#P')
 
 if [ -z "$TMUX" ]; then
-  $EDITOR "+set encoding=$ENCODING" '+set filetype=mail' '+set nofoldenable' '+set nomodifiable' "$TMPFILE"
+  $EDITOR "+set fileencoding=$ENCODING" '+set filetype=mail' '+set nofoldenable' '+set nomodifiable' '+nmap q :q!<cr>' "$TMPFILE"
 elif [ "$COLUMNS" -gt 180 ]; then
-  tmux split-window -p 50 -h \
-    $EDITOR "+set encoding=$ENCODING" '+set filetype=mail' '+set nofoldenable' '+set nomodifiable' "$TMPFILE"
+  tmux split-pane -t"$PANE" -p 50 -h $EDITOR "+set fileencoding=$ENCODING" '+set filetype=mail' '+set nofoldenable' '+set nomodifiable' '+nmap q :q!<cr>' "$TMPFILE"
 else
-  tmux split-window -p 50 -v \
-    $EDITOR "+set encoding=$ENCODING" '+set filetype=mail' '+set nofoldenable' '+set nomodifiable' "$TMPFILE"
+  tmux split-pane -t"$PANE" -p 50 -v $EDITOR "+set fileencoding=$ENCODING" '+set filetype=mail' '+set nofoldenable' '+set nomodifiable' '+nmap q :q!<cr>' "$TMPFILE"
 fi
