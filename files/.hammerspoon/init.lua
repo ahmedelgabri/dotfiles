f062e6c8bbd1c80f50010954fc6df3dfe198bab2
Ahmed El Gabri <ahmed@gabri.me>
2018-12-08 16:17:40 +0100

    hammerspoon: Update SpoonInstall & use URLDispatcher spoon

--- files/.hammerspoon/init.lua
+++ files/.hammerspoon/init.lua
@@ -1,11 +1,35 @@
 -- Ensure the IPC command line client is available
 hs.ipc.cliInstall()
 hs.application.enableSpotlightForNameSearches(true)
+hs.loadSpoon('SpoonInstall')
 
 require 'mappings'
 require 'window-managment'
 require 'layout'
 amphetamine = require 'amphetamine'
+
+spoon.SpoonInstall.use_syncinstall = true
+
+spoon.SpoonInstall:andUse('URLDispatcher',
+  {
+    config = {
+      url_patterns = {
+        { 'https?://jira.atlightspeed.net', 'com.google.Chrome' },
+        { 'https?://confluence.atlightspeed.net', 'com.google.Chrome' },
+        { 'https?://%w-%.-github%.com/SEOshop',  'com.google.Chrome' },
+        { 'https?://github.com/merchantos',  'com.google.Chrome' },
+        { 'https?://github.com/lightspeedretail',  'com.google.Chrome' },
+        { 'https?://circleci.com/gh/lightspeedretail',  'com.google.Chrome' },
+        { 'https?://circleci.com/gh/SEOshop',  'com.google.Chrome' },
+        { 'https?://circleci.com/gh/merchantos',  'com.google.Chrome' },
+      },
+      default_handler = 'com.google.Chrome.Canary'
+    },
+    start = true
+  }
+)
+
+
 --
 -- Auto-reload config on change.
 --
