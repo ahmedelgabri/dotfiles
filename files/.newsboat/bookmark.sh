468c5c35b7da612dbf88623a621b4f5a17ef69f1
Ahmed El Gabri <ahmed@gabri.me>
2019-02-23 14:35:49 +0100

    RSS: save bookmarks to file & on pocket. Add new feeds

--- files/.newsboat/bookmark.sh
+++ files/.newsboat/bookmark.sh
@@ -2,12 +2,13 @@
 
 [ "$#" -eq 0 ] && exit 1
 if [ -n $(command -v curl) ]; then
-  url=$(curl -sIL -o /dev/null -w '%{url_effective}' "$1")
+  URL=$(curl -sIL -o /dev/null -w '%{url_effective}' "$1")
 else
-  url="$1"
+  URL="$1"
 fi
-url=$(echo "${url}" | perl -p -e 's/(\?|\&)?utm_[a-z]+=[^\&]+//g;' -e 's/(#|\&)?utm_[a-z]+=[^\&]+//g;')
-title="$2"
-description="$3"
+URL=$(echo "${url}" | perl -p -e 's/(\?|\&)?utm_[a-z]+=[^\&]+//g;' -e 's/(#|\&)?utm_[a-z]+=[^\&]+//g;')
+TITLE="$2"
 
-grep -q "${url}\t${title}\t${description}" ~/Box\ Sync/saved-articles/bookmarks.txt || echo "${url}\t${title}\t${description}" >> ~/Box\ Sync/saved-articles/bookmarks.txt
+grep -q "${TITLE}: ${URL}" ~/Box\ Sync/saved-articles/bookmarks.md || echo "${TITLE}: ${URL}" >>~/Box\ Sync/saved-articles/bookmarks.md
+
+./getpocket.com/send-to-pocket.sh "$URL" "$TITLE"
