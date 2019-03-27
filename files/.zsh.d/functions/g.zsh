0f505525519bbfa7fe3f18eec8b12c19af719d15
Ahmed El Gabri <ahmed@gabri.me>
2019-03-27 22:00:22 +0100

    bin: show last commit info in the g function

--- files/.zsh.d/functions/g.zsh
+++ files/.zsh.d/functions/g.zsh
@@ -22,12 +22,14 @@ function g() {
   if [[ $# > 0 ]]; then
     git "$@"
   else
-    echo "Last commit: $(time_since_last_commit) ago"
+    echo "$(time_since_last_commit)\n"
     git status --short --branch
   fi
 }
 
 function time_since_last_commit() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  git log -1 --pretty=format:"%ar" | sed 's/\([0-9]*\) \(.\).*/\1\2/'
+  local ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  local LAST_COMMIT=$(git log --color=always -n 1 --pretty=format:"%C(green)[%cr]%Creset %C(blue)%h%Creset %s - %C(cyan)%aN%Creset" --date=auto:human 2>/dev/null)
+
+  echo "Last commit:\n$LAST_COMMIT"
 }
