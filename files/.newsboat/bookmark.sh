84285ba2fc0f853480d71b4f2048bb11baf73006
Ahmed El Gabri <ahmed@gabri.me>
2019-07-23 21:55:26 +0200

    RSS: new feed & change bookmark format

--- files/.newsboat/bookmark.sh
+++ files/.newsboat/bookmark.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 [ "$#" -eq 0 ] && exit 1
-if [ -n $(command -v curl) ]; then
+if [ -n "$(command -v curl)" ]; then
   URL=$(curl -sIL -o /dev/null -w '%{url_effective}' "$1")
 else
   URL="$1"
@@ -9,6 +9,6 @@ fi
 URL=$(echo "${url}" | perl -p -e 's/(\?|\&)?utm_[a-z]+=[^\&]+//g;' -e 's/(#|\&)?utm_[a-z]+=[^\&]+//g;')
 TITLE="$2"
 
-grep -q "${TITLE}: ${URL}" ~/Box\ Sync/saved-articles/bookmarks.md || echo "${TITLE}: ${URL}" >>~/Box\ Sync/saved-articles/bookmarks.md
+grep -q "[${TITLE}](${URL})" ~/Box\ Sync/saved-articles/bookmarks.txt || echo "[${TITLE}](${URL})" >>~/Box\ Sync/saved-articles/bookmarks.txt
 
 ./getpocket.com/send-to-pocket.sh "$URL" "$TITLE"
