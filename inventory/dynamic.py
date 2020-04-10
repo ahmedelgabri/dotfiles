5d0cdec94cf8b164c53ce797a14c2e338a50d28d
Ahmed El Gabri <ahmed@gabri.me>
2020-04-10 14:00:01 +0200

    ansible: cleanup

--- inventory/dynamic.py
+++ inventory/dynamic.py
@@ -5,7 +5,7 @@ import socket
 
 HOST = socket.gethostname()
 
-if re.search(r"\__CHANGE_ME__\b", HOST):
+if re.search(r"^ahmed-", HOST):
     GROUP = "work"
 elif re.match(r"pandoras-box(?:\.(?:local|lan)?)?\Z", HOST):
     GROUP = "personal"
