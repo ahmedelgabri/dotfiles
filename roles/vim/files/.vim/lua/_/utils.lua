db453b8b1926bd0d52bab39ec08b14738ffcfb38
Ahmed El Gabri <ahmed@gabri.me>
2020-09-18 17:23:37 +0200

    vim: fix utils.lua

--- roles/vim/files/.vim/lua/_/utils.lua
+++ roles/vim/files/.vim/lua/_/utils.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 M.bmap = function(mode, key, result, opts)
-  vim.fn.nvim_buf_set_keymap(0, mode, key, result, opts)
+  vim.api.nvim_buf_set_keymap(0, mode, key, result, opts)
 end
 
 M.gmap = function(mode, key, result, opts)
-  vim.fn.nvim_set_keymap(mode, key, result, opts)
+  vim.api.nvim_set_keymap(mode, key, result, opts)
 end
 
 M.Augroup = function(group, fn)
