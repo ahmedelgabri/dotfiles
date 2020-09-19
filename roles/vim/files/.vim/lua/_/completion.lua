0beb07218c6b1064a8a40cc0f79c58c359935518
Ahmed El Gabri <ahmed@gabri.me>
2020-09-19 17:03:19 +0200

    vim: remove custom completion label list

--- roles/vim/files/.vim/lua/_/completion.lua
+++ roles/vim/files/.vim/lua/_/completion.lua
@@ -31,27 +31,12 @@ M.setup = function()
       numhl = ''
     })
 
-  vim.g.completion_customize_lsp_label = {
-    Function = ' [function]',
-    Method = ' [method]',
-    Reference = ' [refrence]',
-    Enum = ' [enum]',
-    Field = 'ﰠ [field]',
-    Keyword = ' [key]',
-    Variable = ' [variable]',
-    Folder = ' [folder]',
-    Snippet = ' [snippet]',
-    Operator = ' [operator]',
-    Module = ' [module]',
-    Text = 'ﮜ[text]',
-    Class = ' [class]',
-    Interface = ' [interface]'
-  }
-
   vim.g.completion_enable_snippet = 'vim-vsnip'
   vim.g.completion_auto_change_source = 1 -- Change the completion source automatically if no completion availabe
   vim.g.completion_matching_ignore_case = 1
-  vim.g.completion_trigger_on_delete = 1
+  vim.g.completion_trigger_on_delete = 0
+  vim.g.completion_enable_auto_hover = 0
+  vim.g.completion_enable_auto_signature = 0
   vim.g.completion_chain_complete_list = {
     default = {
       default = {
