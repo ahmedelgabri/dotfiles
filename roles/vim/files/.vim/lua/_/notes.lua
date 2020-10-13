4e03c6264af0ed253e298e0693f90b7987166a81
Ahmed El Gabri <ahmed@gabri.me>
2020-10-13 21:49:56 +0200

    vim: fix some bugs in notes plugin

--- roles/vim/files/.vim/lua/_/notes.lua
+++ roles/vim/files/.vim/lua/_/notes.lua
@@ -1,24 +1,25 @@
 local M = {}
 
-local NOTES_DIR = vim.fn.expand('$NOTES_DIR')
 local sep = '_'
 
--- Investigate how to make this work with commands?
-function M.get_notes_completion(ArgLead, CmdLine, CursorPos)
-  return vim.fn.map(vim.fn.getcompletion(M.get_dir() .. '/*/**/', 'dir'), function(_, v) return vim.fn.substitute(v, 'mC^'.. os.getenv('HOME') ..'/', '~/', '') end)
+function M.get_dir()
+  return os.getenv('NOTES_DIR')
 end
 
-function M.get_dir()
-  return NOTES_DIR
+-- Investigate how to make this work with commands?
+function M.get_notes_completion()
+  return vim.fn.map(vim.fn.getcompletion(M.get_dir() .. '/*/**/', 'dir'), function(_, v) return string.gsub(v, M.get_dir() .. '/', '') end)
 end
 
 function M.note_info(f_args)
   local path = M.get_dir() .. '/'
-  local fname = #f_args > 0 and f_args[1]:lower():gsub('%s+', '') or ''
+  local fname = ''
 
-  if #f_args > 0 and string.match(f_args[1], '^~/') == '~/' then
-    path = vim.fn.fnamemodify(f_args[1], ':h') .. '/'
-    fname = vim.fn.fnamemodify(f_args[1], ':t:r'):lower()
+  if #f_args >= 1 then
+    local where = string.gsub(vim.fn.fnamemodify(f_args[1], ':h') .. '/', '^.', '')
+
+    path = path .. where
+    fname = vim.fn.fnamemodify(f_args[1], ':t:r'):lower() or ''
   end
 
   local has_fname = fname ~= ''
@@ -26,18 +27,19 @@ function M.note_info(f_args)
   path = path .. vim.fn.strftime('%Y-%m-%dT%H-%M-%S') .. (has_fname and sep or '') .. fname .. '.md'
   local tail = vim.fn.fnamemodify(path, ':t:r')
 
-  return {path, has_fname and vim.fn.split(tail, sep)[2] or ''}
+  return {path, has_fname and vim.fn.split(tail, sep)[2] or '', vim.fn.strftime('%A, %B %d, %Y, %H:%M')}
 end
 
 function M.note_edit(f_args)
   local data = M.note_info(f_args)
   local path = data[1]
   local fname = data[2]
+  local formatted_date = data[3]
 
   print(path)
   vim.cmd('edit ' .. path)
 
-  local frontmatter = { 'normal ggO---', 'date: ' .. vim.fn.strftime('%A, %B %d, %Y, %H:%M'), 'title: ' .. fname, '---' }
+  local frontmatter = { 'normal ggO---', 'date: ' .. formatted_date, 'title: ' .. fname, '---' }
   vim.cmd(table.concat(frontmatter, '\n'))
 
   vim.cmd('silent! packadd goyo.vim | Goyo')
--- roles/vim/files/.vim/plugin/notes.vim
+++ roles/vim/files/.vim/plugin/notes.vim
@@ -2,11 +2,11 @@
 " https://danishpraka.sh/2020/02/23/journaling-in-vim.html
 
 func! GetNotesCompletion(ArgLead, CmdLine, CursorPos) abort
-  return luaeval("require'_/notes'.get_notes_completion(_A[1], _A[2], _A[3])", [ArgLead, CmdLine, CursorPos])
+  return luaeval("require'_.notes'.get_notes_completion()")
 endfunc
 
-command! -complete=customlist,GetNotesCompletion -nargs=* Note lua require'_/notes'.note_edit({<f-args>})
-command! -nargs=* Wiki lua require'_/notes'.wiki_edit({<f-args>})
-command! -bang Notes lua require'_/notes'.search_notes()
+command! -complete=customlist,GetNotesCompletion -nargs=* Note lua require'_.notes'.note_edit({<f-args>})
+command! -nargs=* Wiki lua require'_.notes'.wiki_edit({<f-args>})
+command! -bang Notes lua require'_.notes'.search_notes()
 
 nnoremap <silent> <leader>sn :Notes<CR>
