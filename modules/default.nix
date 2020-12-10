5420d50854c4f26231ddd5d65ad4cf1197a255a6
Ahmed El Gabri <ahmed@gabri.me>
2020-12-10 23:56:57 +0100

    nix: enable all modules

--- modules/default.nix
+++ modules/default.nix
@@ -31,24 +31,24 @@
 
   my = {
     shell.enable = true;
-    # git.enable = true;
-    # mail = { enable = true; };
-    # gpg.enable = true;
-    # ssh.enable = true;
-    # kitty.enable = true;
-    # alacritty.enable = true;
-    # bat.enable = true;
-    # lf.enable = true;
-    # mpv.enable = true;
-    # newsboat.enable = true;
-    # python.enable = true;
-    # ripgrep.enable = true;
-    # tmux.enable = true;
-    # tuir.enable = true;
-    # youtube-dl.enable = true;
-    # misc.enable = true;
-    # node.enable = true;
-    # macos.enable = pkgs.stdenv.isDarwin;
-    # hammerspoon.enable = pkgs.stdenv.isDarwin;
+    git.enable = true;
+    mail = { enable = true; };
+    gpg.enable = true;
+    ssh.enable = true;
+    kitty.enable = true;
+    alacritty.enable = true;
+    bat.enable = true;
+    lf.enable = true;
+    mpv.enable = true;
+    newsboat.enable = true;
+    python.enable = true;
+    ripgrep.enable = true;
+    tmux.enable = true;
+    tuir.enable = true;
+    youtube-dl.enable = true;
+    misc.enable = true;
+    node.enable = true;
+    macos.enable = pkgs.stdenv.isDarwin;
+    hammerspoon.enable = pkgs.stdenv.isDarwin;
   };
 }
