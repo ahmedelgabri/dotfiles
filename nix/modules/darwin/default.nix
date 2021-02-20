4e5700662df2e4cd0bb2920999e9b8346f8753b1
Ahmed El Gabri <ahmed@gabri.me>
2021-02-20 17:03:03 +0100

    nix: update flakes & fix nix-darwin users issue

--- nix/modules/darwin/default.nix
+++ nix/modules/darwin/default.nix
@@ -1,10 +1,13 @@
 { config, pkgs, ... }:
 
 {
+  users.nix.configureBuildUsers = true;
+
   homebrew.enable = true;
   homebrew.autoUpdate = true;
   homebrew.cleanup = "zap";
   homebrew.global.brewfile = true;
   homebrew.global.noLock = true;
+
   imports = [ ./macos.nix ];
 }
