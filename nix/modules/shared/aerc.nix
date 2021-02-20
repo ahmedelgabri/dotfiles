985d5722a25e4132b262ab8fe33f9afd8a03006c
Ahmed El Gabri <ahmed@gabri.me>
2021-02-20 17:03:35 +0100

    aerc: remove launchd from aerc & keep it in a single place for now

--- nix/modules/shared/aerc.nix
+++ nix/modules/shared/aerc.nix
@@ -51,121 +51,96 @@ in {
   };
 
   config = with lib;
-    mkIf cfg.enable (mkMerge [
-      (mkIf pkgs.stdenv.isDarwin {
-        launchd.user.agents."isync" = {
-          # This will call notmuch `pre-new` hook that will fetch new mail & addresses too
-          # Check `.mail/.notmuch/hooks/`
-          command =
-            "${pkgs.notmuch}/bin/notmuch --config=${xdg.configHome}/notmuch/config new";
-          serviceConfig = {
-            ProcessType = "Background";
-            LowPriorityIO = true;
-            StartInterval = 2 * 60;
-            RunAtLoad = true;
-            KeepAlive = false;
-            StandardOutPath = "${homeDir}/Library/Logs/isync-output.log";
-            StandardErrorPath = "${homeDir}/Library/Logs/isync-error.log";
-            EnvironmentVariables = {
-              "SSL_CERT_FILE" = "/etc/ssl/certs/ca-certificates.crt";
-            };
-          };
-        };
-      })
-      (mkIf pkgs.stdenv.isLinux {
-        # systemd
-      })
-      {
-        users.users.${username} = {
-          packages = with pkgs; [ aerc isync w3m notmuch pass ];
-        };
+    mkIf cfg.enable (mkMerge [{
+      users.users.${username} = {
+        packages = with pkgs; [ aerc isync w3m notmuch pass ];
+      };
 
-        environment.variables = {
-          MAILDIR =
-            "$HOME/.mail"; # will be picked up by .notmuch-config for database.path
-          NOTMUCH_CONFIG = "${xdg.configHome}/notmuch/config";
-          # MAILCAP="${xdg.configHome}/mailcap"; # elinks, w3m
-          # MAILCAPS="$MAILCAP";   # Mutt, pine
-        };
+      environment.variables = {
+        MAILDIR =
+          "$HOME/.mail"; # will be picked up by .notmuch-config for database.path
+        NOTMUCH_CONFIG = "${xdg.configHome}/notmuch/config";
+        # MAILCAP="${xdg.configHome}/mailcap"; # elinks, w3m
+        # MAILCAPS="$MAILCAP";   # Mutt, pine
+      };
 
-        home-manager = {
-          users.${username} = {
-            home = {
-              file = {
-                ".config/aerc/stylesets" = {
-                  recursive = true;
-                  source = ../../../config/aerc/stylesets;
-                };
+      home-manager = {
+        users.${username} = {
+          home = {
+            file = {
+              ".config/aerc/stylesets" = {
+                recursive = true;
+                source = ../../../config/aerc/stylesets;
+              };
 
-                ".config/aerc/binds.conf" = {
-                  source = ../../../config/aerc/binds.conf;
-                };
+              ".config/aerc/binds.conf" = {
+                source = ../../../config/aerc/binds.conf;
+              };
 
-                ".config/aerc/accounts.conf" = {
-                  text = ''
-                    [${cfg.account.name}]
-                    smtp-starttls     = yes
-                    from              = ${config.settings.name} <${config.settings.email}>
-                    source            = imaps://${cfg.pass.account}@${cfg.imap_server}
-                    source-cred-cmd   = pass email/${cfg.pass.name}
-                    outgoing          = smtps+plain://${cfg.pass.account}@${cfg.smtp_server}
-                    outgoing-cred-cmd = pass email/${cfg.pass.name}
-                    copy-to           = Sent
-                    postpone          = Drafts
-                    archive           = Archive
-                    folders-sort      = INBOX, Starred, Drafts, Sent, Trash, Archive, Spam
-                    source            = maildir://~/.mail/${cfg.account.type}
-                    # signature-file    = ~/.signature.local
+              ".config/aerc/accounts.conf" = {
+                text = ''
+                  [${cfg.account.name}]
+                  smtp-starttls     = yes
+                  from              = ${config.settings.name} <${config.settings.email}>
+                  source            = imaps://${cfg.pass.account}@${cfg.imap_server}
+                  source-cred-cmd   = pass email/${cfg.pass.name}
+                  outgoing          = smtps+plain://${cfg.pass.account}@${cfg.smtp_server}
+                  outgoing-cred-cmd = pass email/${cfg.pass.name}
+                  copy-to           = Sent
+                  postpone          = Drafts
+                  archive           = Archive
+                  folders-sort      = INBOX, Starred, Drafts, Sent, Trash, Archive, Spam
+                  source            = maildir://~/.mail/${cfg.account.type}
+                  # signature-file    = ~/.signature.local
 
-                    # [notmuch]
-                    # source            = notmuch://~/.mail/
-                    # query-map         = ~/.config/aerc/querymap'';
-                };
+                  # [notmuch]
+                  # source            = notmuch://~/.mail/
+                  # query-map         = ~/.config/aerc/querymap'';
+              };
 
-                ".config/aerc/querymap" = {
-                  source = ../../../config/aerc/querymap;
-                };
+              ".config/aerc/querymap" = {
+                source = ../../../config/aerc/querymap;
+              };
 
-                ".config/aerc/aerc.conf" = {
-                  text = ''
-                    ${builtins.readFile ../../../config/aerc/aerc.conf}
+              ".config/aerc/aerc.conf" = {
+                text = ''
+                  ${builtins.readFile ../../../config/aerc/aerc.conf}
 
-                    [filters]
-                    #
-                    # Filters allow you to pipe an email body through a shell command to render
-                    # certain emails differently, e.g. highlighting them with ANSI escape codes.
-                    #
-                    # The first filter which matches the email's mimetype will be used, so order
-                    # them from most to least specific.
-                    #
-                    # You can also match on non-mimetypes, by prefixing with the header to match
-                    # against (non-case-sensitive) and a comma, e.g. subject,text will match a
-                    # subject which contains "text". Use header,~regex to match against a regex.
-                    subject,~^\[PATCH=awk -f ${pkgs.aerc}/share/aerc/filters/hldiff
-                    subject,~^\[RFC=awk -f ${pkgs.aerc}/share/aerc/filters/hldiff
-                    # text/html=${pkgs.aerc}/share/aerc/filters/html
-                    text/plain=par -w 120 -
-                    text/html=w3m -T text/html -cols 120 -dump -o display_image=false -o display_link_number=true
-                    text/*=awk -f ${pkgs.aerc}/share/aerc/filters/plaintext
-                    image/*=chafa -
-                    # image/*=catimg -w $(tput cols) -
+                  [filters]
+                  #
+                  # Filters allow you to pipe an email body through a shell command to render
+                  # certain emails differently, e.g. highlighting them with ANSI escape codes.
+                  #
+                  # The first filter which matches the email's mimetype will be used, so order
+                  # them from most to least specific.
+                  #
+                  # You can also match on non-mimetypes, by prefixing with the header to match
+                  # against (non-case-sensitive) and a comma, e.g. subject,text will match a
+                  # subject which contains "text". Use header,~regex to match against a regex.
+                  subject,~^\[PATCH=awk -f ${pkgs.aerc}/share/aerc/filters/hldiff
+                  subject,~^\[RFC=awk -f ${pkgs.aerc}/share/aerc/filters/hldiff
+                  # text/html=${pkgs.aerc}/share/aerc/filters/html
+                  text/plain=par -w 120 -
+                  text/html=w3m -T text/html -cols 120 -dump -o display_image=false -o display_link_number=true
+                  text/*=awk -f ${pkgs.aerc}/share/aerc/filters/plaintext
+                  image/*=chafa -
+                  # image/*=catimg -w $(tput cols) -
 
-                    [templates]
-                    # Templates are used to populate email bodies automatically.
-                    #
+                  [templates]
+                  # Templates are used to populate email bodies automatically.
+                  #
 
-                    # The directories where the templates are stored. It takes a colon-separated
-                    # list of directories.
-                    #
-                    # default: /usr/local/share/aerc/templates/
-                    template-dirs=${pkgs.aerc}/share/aerc/templates/'';
-                };
+                  # The directories where the templates are stored. It takes a colon-separated
+                  # list of directories.
+                  #
+                  # default: /usr/local/share/aerc/templates/
+                  template-dirs=${pkgs.aerc}/share/aerc/templates/'';
               };
             };
           };
         };
-      }
-    ]
+      };
+    }]
 
     );
 }
