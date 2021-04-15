e0b467164c02c5b1b4729e821926cf434b782a5d
Ahmed El Gabri <ahmed@gabri.me>
2021-04-15 23:09:47 +0200

    nix: enable avahi service on NixOS

--- nix/hosts/nixos/default.nix
+++ nix/hosts/nixos/default.nix
@@ -199,6 +199,8 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  services.avahi.enable = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
