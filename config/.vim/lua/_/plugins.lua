8d401186f3a2409d61a6c5513e901e5afaa7896a
Ahmed El Gabri <ahmed@gabri.me>
2021-04-25 14:47:24 +0200

    vim: update conjure

--- config/.vim/lua/_/plugins.lua
+++ config/.vim/lua/_/plugins.lua
@@ -201,7 +201,7 @@ local plugins = {
   {"https://github.com/jez/vim-github-hub"},
   -- Clojure
   {"https://github.com/guns/vim-sexp", ft = lisps},
-  {"https://github.com/Olical/conjure", tag = "v4.17.0", ft = lisps},
+  {"https://github.com/Olical/conjure", tag = "v4.18.0", ft = lisps},
   -- }}}
 
   -- Linters & Code quality {{{
