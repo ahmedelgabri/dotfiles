a21f46edcb2c2eb33b6a043e71f1cb81bb31cb36
Ahmed El Gabri <ahmed@gabri.me>
2021-04-27 18:38:49 +0200

    vim: update snippets setup

--- config/.vim/lua/_/lsp.lua
+++ config/.vim/lua/_/lsp.lua
@@ -185,6 +185,13 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    "documentation",
+    "detail",
+    "additionalTextEdits"
+  }
+}
 
 require("nlua.lsp.nvim").setup(
   nvim_lsp,
