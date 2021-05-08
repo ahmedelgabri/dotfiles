b12bc2db5ae2f195be9ddbbaa4c8bd5084de1890
Ahmed El Gabri <ahmed@gabri.me>
2021-05-08 19:06:08 +0200

    nix: clean up comments

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 #
 # https://nixcloud.io/ for Nix syntax
 # https://nix.dev/
+# https://nixos.org/guides/nix-pills/
 # https://nix-community.github.io/awesome-nix/
 # https://discourse.nixos.org/t/home-manager-equivalent-of-apt-upgrade/8424/3
 # https://www.reddit.com/r/NixOS/comments/jmom4h/new_neofetch_nixos_logo/gayfal2/
@@ -11,25 +12,7 @@
 # https://www.reddit.com/r/NixOS/comments/k9xwht/best_resources_for_learning_nixos/
 # https://www.reddit.com/r/NixOS/comments/k8zobm/nixos_preferred_packages_flow/
 # https://www.reddit.com/r/NixOS/comments/j4k2zz/does_anyone_use_flakes_to_manage_their_entire/
-#
-# Sample repos
-# https://github.com/teoljungberg/dotfiles/tree/master/nixpkgs (contains custom hammerspoon & vim )
-# https://github.com/jwiegley/nix-config (nice example)
-# https://github.com/hardselius/dotfiles (good readme on steps to do for install)
-# https://github.com/nuance/dotfiles
-#
-# With flakes
-# https://github.com/hlissner/dotfiles
-# https://github.com/mjlbach/nix-dotfiles
-# https://github.com/thpham/nix-configs/blob/e46a15f69f/default.nix (nice example of how to build)
-# https://github.com/sandhose/nixconf
-# https://github.com/cideM/dotfiles (darwin, NixOS, home-manager)
-# https://github.com/monadplus/nixconfig
-# https://github.com/jamesottaway/dotfiles (Darwin, NixOS, home-manager)
-# https://github.com/malob/nixpkgs (Darwin, home-manager, homebrew in nix & PAM, this is a great example)
-# https://github.com/kclejeune/system (nice example)
-# https://github.com/mrkuz/nixos
-#
+# https://serokell.io/blog/practical-nix-flakes
 
 {
   description = "~ 🍭 ~";
