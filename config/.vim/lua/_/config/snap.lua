7e1f3499de6736f2dfe37149c771c67a806e4530
Ahmed El Gabri <ahmed@gabri.me>
2021-06-20 09:56:29 +0200

    vim: testing new snap configs

--- config/.vim/lua/_/config/snap.lua
+++ config/.vim/lua/_/config/snap.lua
@@ -1,29 +1,27 @@
 local snap = require "snap"
 local M = {}
 
-local fzf = snap.get "consumer.fzf"
-local limit = snap.get "consumer.limit"
-local producer_file = snap.get "producer.fd.file"
-local producer_vimgrep = snap.get "producer.ripgrep.vimgrep"
-local producer_buffer = snap.get "producer.vim.buffer"
-local producer_oldfile = snap.get "producer.vim.oldfile"
-local select_file = snap.get "select.file"
-local select_vimgrep = snap.get "select.vimgrep"
-local preview_file = snap.get "preview.file"
-local preview_vimgrep = snap.get "preview.vimgrep"
-
 function M.setup()
   snap.register.map(
     {"n"},
     {"<Leader><Leader>"},
     function()
+      local fd_args = vim.split(os.getenv("FZF_DEFAULT_COMMAND"), " ")
+      -- Mutate the table & remove the first item, that's the fd command
+      table.remove(fd_args, 1)
+
       snap.run(
         {
           prompt = "",
-          producer = fzf(producer_file.args {"-H"}),
-          select = select_file.select,
-          multiselect = select_file.multiselect,
-          views = {preview_file}
+          producer = snap.get "consumer.fzf"(
+            snap.get "consumer.try"(
+              -- snap.get "producer.git.file",
+              snap.get "producer.fd.file".args(fd_args)
+            )
+          ),
+          select = snap.get "select.file".select,
+          multiselect = snap.get "select.file".multiselect,
+          views = {snap.get "preview.file"}
         }
       )
     end
@@ -35,9 +33,45 @@ function M.setup()
     function()
       snap.run {
         prompt = "?",
-        producer = fzf(snap.get "producer.vim.help"),
+        producer = snap.get "consumer.fzf"(snap.get "producer.vim.help"),
         select = snap.get "select.help".select,
         views = {snap.get "preview.help"}
+      }
+    end
+  )
+
+  snap.register.map(
+    {"n"},
+    {"<localleader>f"},
+    function()
+      snap.run {
+        producer = snap.get "producer.ripgrep.vimgrep",
+        steps = {
+          {
+            consumer = snap.get "consumer.fzf",
+            config = {prompt = "FZF>"}
+          }
+        },
+        select = snap.get "select.file".select,
+        multiselect = snap.get "select.file".multiselect,
+        views = {snap.get "preview.file"}
+      }
+    end
+  )
+
+  snap.register.map(
+    {"n"},
+    {"<localleader>sn"},
+    function()
+      snap.run {
+        producer = snap.get "consumer.fzf"(
+          snap.get "consumer.combine"(
+            snap.get "producer.ripgrep.file".args({}, os.getenv("NOTES_DIR"))
+          )
+        ),
+        select = snap.get "select.vimgrep".select,
+        multiselect = snap.get "select.vimgrep".multiselect,
+        views = {snap.get "preview.file"}
       }
     end
   )
@@ -48,10 +82,10 @@ function M.setup()
   --     snap.run(
   --       {
   --         prompt = "Grep",
-  --         producer = limit(10000, producer_vimgrep),
-  --         select = select_vimgrep.select,
-  --         multiselect = select_vimgrep.multiselect,
-  --         views = {preview_vimgrep}
+  --         producer = snap.get "consumer.limit"(10000, snap.get "producer.ripgrep.vimgrep"),
+  --         select = snap.get "select.vimgrep".select,
+  --         multiselect = snap.get "select.vimgrep".multiselect,
+  --         views = {snap.get "preview.vimgrep"}
   --       }
   --     )
   --   end
@@ -64,10 +98,10 @@ function M.setup()
       snap.run(
         {
           prompt = "Buffers",
-          producer = fzf(producer_buffer),
-          select = select_file.select,
-          multiselect = select_file.multiselect,
-          views = {preview_file}
+          producer = snap.get "consumer.fzf"(snap.get "producer.vim.buffer"),
+          select = snap.get "select.file".select,
+          multiselect = snap.get "select.file".multiselect,
+          views = {snap.get "preview.file"}
         }
       )
     end
@@ -80,10 +114,10 @@ function M.setup()
       snap.run(
         {
           prompt = "Oldfiles",
-          producer = fzf(producer_oldfile),
-          select = select_file.select,
-          multiselect = select_file.multiselect,
-          views = {preview_file}
+          producer = snap.get "consumer.fzf"(snap.get "producer.vim.oldfile"),
+          select = snap.get "select.file".select,
+          multiselect = snap.get "select.file".multiselect,
+          views = {snap.get "preview.file"}
         }
       )
     end
@@ -96,9 +130,9 @@ function M.setup()
   --     snap.run(
   --       {
   --         prompt = "Grep",
-  --         producer = limit(10000, producer_vimgrep),
-  --         select = select_vimgrep.select,
-  --         multiselect = select_vimgrep.multiselect,
+  --         producer = snap.get "consumer.limit"(10000, snap.get "producer.ripgrep.vimgrep"),
+  --         select = snap.get "select.vimgrep".select,
+  --         multiselect = snap.get "select.vimgrep".multiselect,
   --         initial_filter = vim.fn.expand("<cword>")
   --       }
   --     )
