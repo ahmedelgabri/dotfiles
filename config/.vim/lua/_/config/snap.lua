d13872b003cc3ffc18688d86d846af5d386b03e1
Ahmed El Gabri <ahmed@gabri.me>
2021-06-27 14:01:41 +0200

    vim: add snap config

--- config/.vim/lua/_/config/snap.lua
+++ config/.vim/lua/_/config/snap.lua
@@ -1,41 +1,109 @@
 local snap = require "snap"
+local config = snap.config
+local notes = require "_.notes"
 local M = {}
 
-function M.setup()
-  snap.register.map(
-    {"n"},
-    {"<Leader><Leader>"},
-    function()
-      local fd_args = vim.split(os.getenv("FZF_DEFAULT_COMMAND"), " ")
-      -- Mutate the table & remove the first item, that's the fd command
-      table.remove(fd_args, 1)
+local fd_args = vim.split(os.getenv("FZF_DEFAULT_COMMAND"), " ")
+-- Mutate the table & remove the first item, that's the fd command
+table.remove(fd_args, 1)
 
-      snap.run(
-        {
-          prompt = "",
-          producer = snap.get "consumer.fzf"(
-            snap.get "consumer.try"(
-              -- snap.get "producer.git.file",
-              snap.get "producer.fd.file".args(fd_args)
-            )
-          ),
-          select = snap.get "select.file".select,
-          multiselect = snap.get "select.file".multiselect,
-          views = {snap.get "preview.file"}
-        }
-      )
-    end
+local custom_mappings = {
+  ["view-toggle-hide"] = {"?"}
+}
+
+local defaults = {
+  reverse = true,
+  prompt = "",
+  suffix = "",
+  -- consumer = "fzy",
+  mappings = custom_mappings,
+  preview_min_width = 80
+}
+
+local function file(opts)
+  local options = vim.tbl_extend("force", defaults, opts or {})
+
+  return config.file:with(options)
+end
+
+local vimgrep =
+  config.vimgrep:with(
+  vim.tbl_extend(
+    "force",
+    defaults,
+    {
+      prompt = " /",
+      -- filter_with = "cword",
+      limit = 50000
+    }
   )
+)
+
+local notes_producer =
+  snap.get "consumer.limit"(
+  50000,
+  function(request)
+    return snap.get "producer.ripgrep.general"(
+      request,
+      {
+        absolute = true,
+        args = {
+          "--line-buffered",
+          "-M",
+          "100",
+          "--vimgrep",
+          request.filter
+        },
+        cwd = snap.sync(notes.get_dir)
+      }
+    )
+  end
+)
+
+function M.setup()
+  snap.maps {
+    {
+      "<leader><leader>",
+      file() {
+        args = fd_args,
+        try = {
+          -- "git.file",
+          "fd.file"
+        }
+      },
+      {command = "files"}
+    },
+    {"<leader>fg", file() {producer = "git.file"}, {command = "git.files"}},
+    {
+      "<leader>b",
+      file({preview = false}) {producer = "vim.buffer"},
+      {command = "buffers"}
+    },
+    {"<leader>ff", vimgrep {}, {command = "grep"}},
+    {
+      "<leader>fo",
+      file({preview = false}) {producer = "vim.oldfile"},
+      {command = "oldfiles"}
+    },
+    {
+      "<localleader>c",
+      file() {
+        combine = {"vim.buffer", "vim.oldfile"}
+      }
+    }
+  }
 
   snap.register.map(
     {"n"},
-    {"<Leader>h"},
+    {"<leader>h"},
     function()
       snap.run {
+        reverse = true,
         prompt = "?",
         producer = snap.get "consumer.fzf"(snap.get "producer.vim.help"),
         select = snap.get "select.help".select,
-        views = {snap.get "preview.help"}
+        views = {snap.get "preview.help"},
+        mappings = custom_mappings
       }
     end
   )
@@ -45,16 +113,19 @@ function M.setup()
     {"<localleader>f"},
     function()
       snap.run {
-        producer = snap.get "producer.ripgrep.vimgrep",
+        producer = notes_producer,
         steps = {
           {
             consumer = snap.get "consumer.fzf",
             config = {prompt = "FZF>"}
+            -- consumer = snap.get "consumer.fzy",
+            -- config = {prompt = "FZY>"}
           }
         },
         select = snap.get "select.file".select,
         multiselect = snap.get "select.file".multiselect,
-        views = {snap.get "preview.file"}
+        views = {snap.get "preview.file"},
+        mappings = custom_mappings
       }
     end
   )
@@ -64,80 +135,17 @@ function M.setup()
     {"<localleader>sn"},
     function()
       snap.run {
-        producer = snap.get "consumer.fzf"(
-          snap.get "consumer.combine"(
-            snap.get "producer.ripgrep.file".args({}, os.getenv("NOTES_DIR"))
-          )
-        ),
+        prompt = "notes:",
+        reverse = true,
+        producer = notes_producer,
         select = snap.get "select.vimgrep".select,
         multiselect = snap.get "select.vimgrep".multiselect,
-        views = {snap.get "preview.file"}
+        views = {snap.get "preview.vimgrep"},
+        -- initial_filter = vim.fn.expand("<cword>"),
+        mappings = custom_mappings
       }
     end
   )
-  -- snap.register.map(
-  --   {"n"},
-  --   {"<Leader>ff"},
-  --   function()
-  --     snap.run(
-  --       {
-  --         prompt = "Grep",
-  --         producer = snap.get "consumer.limit"(10000, snap.get "producer.ripgrep.vimgrep"),
-  --         select = snap.get "select.vimgrep".select,
-  --         multiselect = snap.get "select.vimgrep".multiselect,
-  --         views = {snap.get "preview.vimgrep"}
-  --       }
-  --     )
-  --   end
-  -- )
-
-  snap.register.map(
-    {"n"},
-    {"<Leader>b"},
-    function()
-      snap.run(
-        {
-          prompt = "Buffers",
-          producer = snap.get "consumer.fzf"(snap.get "producer.vim.buffer"),
-          select = snap.get "select.file".select,
-          multiselect = snap.get "select.file".multiselect,
-          views = {snap.get "preview.file"}
-        }
-      )
-    end
-  )
-
-  snap.register.map(
-    {"n"},
-    {"<Leader>fo"},
-    function()
-      snap.run(
-        {
-          prompt = "Oldfiles",
-          producer = snap.get "consumer.fzf"(snap.get "producer.vim.oldfile"),
-          select = snap.get "select.file".select,
-          multiselect = snap.get "select.file".multiselect,
-          views = {snap.get "preview.file"}
-        }
-      )
-    end
-  )
-
-  -- snap.register.map(
-  --   {"n"},
-  --   {"<Leader>fb"},
-  --   function()
-  --     snap.run(
-  --       {
-  --         prompt = "Grep",
-  --         producer = snap.get "consumer.limit"(10000, snap.get "producer.ripgrep.vimgrep"),
-  --         select = snap.get "select.vimgrep".select,
-  --         multiselect = snap.get "select.vimgrep".multiselect,
-  --         initial_filter = vim.fn.expand("<cword>")
-  --       }
-  --     )
-  --   end
-  -- )
 end
 
 return M
--- config/.vim/plugin/color.vim
+++ config/.vim/plugin/color.vim
@@ -46,4 +46,10 @@ augroup MyCustomColors
         \| hi! link Todo Comment
         \| hi! link Conceal NonText
         \| hi! link Error ErrorMsg
+        \| hi! link SnapSelect CursorLine
+        \| hi! link SnapMultiSelect DiffAdd
+        \| hi! link SnapNormal Normal
+        \| hi! link SnapBorder SnapNormal
+        \| hi! link SnapPrompt NonText
+        \| hi! link SnapPosition DiffText
 augroup END
