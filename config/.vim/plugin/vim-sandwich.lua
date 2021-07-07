d627184d0d219f6a37c10fb585bb77699736888f
Ahmed El Gabri <ahmed@gabri.me>
2021-07-07 23:46:48 +0200

    vim: fix vim-sandwitch

--- config/.vim/plugin/vim-sandwich.lua
+++ config/.vim/plugin/vim-sandwich.lua
@@ -1,43 +1,42 @@
-local utils = require "_.utils"
-
-if not utils.plugin_loaded("surround.vim") then
-  return
-end
-
-vim.api.nvim_command("runtime macros/sandwich/keymap/surround.vim")
-vim.g["sandwich#recipes"] =
-  vim.tbl_extend(
-  "force",
-  vim.g["sandwich#recipes"],
-  {
-    {
-      buns = {[[/\*\s*]], [[\s*\*/]]},
-      regex = 1,
-      filetype = {
-        "typescript",
-        "typescriptreact",
-        "typescript.tsx",
-        "javascript",
-        "javascriptreact",
-        "javascript.jsx"
-      },
-      input = {"/"}
-    },
-    {
-      buns = {"${", "}"},
-      filetype = {
-        "typescript",
-        "typescriptreact",
-        "typescript.tsx",
-        "javascript",
-        "javascriptreact",
-        "javascript.jsx",
-        "zsh",
-        "bash",
-        "shell",
-        "nix"
-      },
-      input = {"$"}
-    }
-  }
+local ok =
+  pcall(
+  function()
+    vim.api.nvim_command("runtime macros/sandwich/keymap/surround.vim")
+    vim.g["sandwich#recipes"] =
+      vim.tbl_extend(
+      "force",
+      vim.g["sandwich#recipes"],
+      {
+        {
+          buns = {[[/\*\s*]], [[\s*\*/]]},
+          regex = 1,
+          filetype = {
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx"
+          },
+          input = {"/"}
+        },
+        {
+          buns = {"${", "}"},
+          filetype = {
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "zsh",
+            "bash",
+            "shell",
+            "nix"
+          },
+          input = {"$"}
+        }
+      }
+    )
+  end
 )
