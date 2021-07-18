33a8f09e5d6c522cc959894a93f6f034b7d642fe
Ahmed El Gabri <ahmed@gabri.me>
2021-07-18 17:18:52 +0200

    vim: Enable treesitter move

--- config/.vim/plugin/treesitter.lua
+++ config/.vim/plugin/treesitter.lua
@@ -29,6 +29,40 @@ require('nvim-treesitter.configs').setup {
     -- https://www.reddit.com/r/neovim/comments/ok9frp/v05_treesitter_does_anyone_have_python_indent/h57kxuv/?context=3
     additional_vim_regex_highlighting = { 'python' },
   },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+        ['aC'] = '@conditional.outer',
+        ['iC'] = '@conditional.inner',
+      },
+    },
+  },
+  move = {
+    enable = true,
+    set_jumps = true, -- whether to set jumps in the jumplist
+    goto_next_start = {
+      [']m'] = '@function.outer',
+      [']]'] = '@class.outer',
+    },
+    goto_next_end = {
+      [']M'] = '@function.outer',
+      [']['] = '@class.outer',
+    },
+    goto_previous_start = {
+      ['[m'] = '@function.outer',
+      ['[['] = '@class.outer',
+    },
+    goto_previous_end = {
+      ['[M'] = '@function.outer',
+      ['[]'] = '@class.outer',
+    },
+  },
   rainbow = {
     enable = true,
     -- Enable only for lisp like languages
@@ -41,19 +75,6 @@ require('nvim-treesitter.configs').setup {
   },
   autopairs = {
     enable = true,
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-        ['aC'] = '@conditional.outer',
-        ['iC'] = '@conditional.inner',
-      },
-    },
   },
   playground = {
     enable = true,
