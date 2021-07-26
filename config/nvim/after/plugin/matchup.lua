140e59dd92f6c395394b525769563bcd9c11cf4c
Ahmed El Gabri <ahmed@gabri.me>
2021-07-26 21:34:46 +0200

    vim: remove old plugin config file

--- config/nvim/after/plugin/matchup.lua
+++
@@ -1,8 +0,0 @@
-if vim.fn.exists ':Matchup' == 0 then
-  return
-end
-
-vim.g.matchup_surround_enabled = 1
-vim.g.matchup_transmute_enabled = 1
-vim.g.matchup_matchpref_html_nolists = 1
-vim.g.matchup_matchparen_deferred = 1
