c314622b9d1dc56bb946e72eb5d4695fa76986cf
Ahmed El Gabri <ahmed@gabri.me>
2021-09-02 21:14:13 +0200

    vim: update vim-sandwitch config

--- config/nvim/plugin/vim-sandwich.lua
+++ config/nvim/plugin/vim-sandwich.lua
@@ -1,39 +1,38 @@
 local ok = pcall(function()
   vim.api.nvim_command 'runtime macros/sandwich/keymap/surround.vim'
-  vim.g['sandwich#recipes'] =
-    vim.tbl_extend(
-      'force',
-      vim.g['sandwich#recipes'],
+  vim.g['sandwich#recipes'] = vim.tbl_extend(
+    'force',
+    vim.deepcopy(vim.g['sandwich#default_recipes']),
+    {
       {
-        {
-          buns = { [[/\*\s*]], [[\s*\*/]] },
-          regex = 1,
-          filetype = {
-            'typescript',
-            'typescriptreact',
-            'typescript.tsx',
-            'javascript',
-            'javascriptreact',
-            'javascript.jsx',
-          },
-          input = { '/' },
+        buns = { [[/\*\s*]], [[\s*\*/]] },
+        regex = 1,
+        filetype = {
+          'typescript',
+          'typescriptreact',
+          'typescript.tsx',
+          'javascript',
+          'javascriptreact',
+          'javascript.jsx',
         },
-        {
-          buns = { '${', '}' },
-          filetype = {
-            'typescript',
-            'typescriptreact',
-            'typescript.tsx',
-            'javascript',
-            'javascriptreact',
-            'javascript.jsx',
-            'zsh',
-            'bash',
-            'shell',
-            'nix',
-          },
-          input = { '$' },
+        input = { '/' },
+      },
+      {
+        buns = { '${', '}' },
+        filetype = {
+          'typescript',
+          'typescriptreact',
+          'typescript.tsx',
+          'javascript',
+          'javascriptreact',
+          'javascript.jsx',
+          'zsh',
+          'bash',
+          'shell',
+          'nix',
         },
-      }
-    )
+        input = { '$' },
+      },
+    }
+  )
 end)
