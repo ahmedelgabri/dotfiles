38d13691a0c9b19e58a750b89ed4911cf504d99d
Ahmed El Gabri <ahmed@gabri.me>
2021-10-03 13:53:37 +0200

    vim: disable change to dir in startify

--- config/nvim/lua/_/config/startify.lua
+++ config/nvim/lua/_/config/startify.lua
@@ -4,6 +4,7 @@ return function()
   vim.g.startify_padding_left = 5
   vim.g.startify_relative_path = 1
   vim.g.startify_fortune_use_unicode = 1
+  vim.g.startify_change_to_dir = 0
   vim.g.startify_change_to_vcs_root = 1
   vim.g.startify_update_oldfiles = 1
   vim.g.startify_use_env = 1
