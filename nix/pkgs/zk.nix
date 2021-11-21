3dbafc4b5c3ca6a721936da91e5e96d92f1f5ca1
Ahmed El Gabri <ahmed@gabri.me>
2021-11-21 19:30:47 +0100

    nix: update zk

--- nix/pkgs/zk.nix
+++ nix/pkgs/zk.nix
@@ -2,7 +2,7 @@
 
 buildGoModule rec {
   pname = "zk";
-  version = "0.7.0";
+  version = "0.8.0";
 
   # TODO is it possibe to pin the hash in flake.lock?
   # This should be doable with https://github.com/tweag/gomod2nix
