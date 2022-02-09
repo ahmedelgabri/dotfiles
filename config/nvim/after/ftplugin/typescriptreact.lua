54c1d70e5139570a7eb09836ae82527049bf3884
Ahmed El Gabri <ahmed@gabri.me>
2022-02-09 22:47:24 +0100

    vim: goodbye compound filetypes

--- config/nvim/after/ftplugin/typescriptreact.lua
+++
@@ -1,8 +0,0 @@
--- Work around filetype that landed in upstream Vim here:
--- https://github.com/vim/vim/issues/4830
-vim.fn.execute(
-  string.format(
-    'noautocmd set filetype=%s',
-    vim.fn.substitute(vim.bo.filetype, 'typescriptreact', 'typescript.tsx', '')
-  )
-)
