04296023388225c885e8e47cb14078f4f3706a22
Ahmed El Gabri <ahmed@gabri.me>
2022-03-02 09:13:39 +0100

    vim: fix nvim-tree config

--- config/nvim/lua/_/config/nvim-tree.lua
+++ config/nvim/lua/_/config/nvim-tree.lua
@@ -1,12 +1,6 @@
 return function()
   local map = require '_.utils.map'
 
-  -- 0 is supposed to be the default (ie. don't close tree when opening a file),
-  -- but it closes anyway; see: https://github.com/kyazdani42/nvim-tree.lua/issues/502
-  vim.g.nvim_tree_quit_on_open = 0
-
-  vim.g.nvim_tree_disable_window_picker = 1
-
   vim.g.nvim_tree_indent_markers = 1
 
   vim.g.nvim_tree_show_icons = {
@@ -29,6 +23,14 @@ return function()
     auto_close = true,
     update_focused_file = {
       enable = true,
+    },
+    actions = {
+      open_file = {
+        quit_on_open = false,
+        window_picker = {
+          enable = true,
+        },
+      },
     },
     -- vim-fugitive :GBrowse depends on netrw & this has to be set as early as possible
     -- maybe switch to https://github.com/ruifm/gitlinker.nvim?
