3be9753c4ec015261dbf855a3c0904405a5d397a
Ahmed El Gabri <ahmed@gabri.me>
2022-03-17 21:22:46 +0100

    lf: update preview

--- config/lf/preview.sh
+++ config/lf/preview.sh
@@ -2,18 +2,19 @@
 
 FILE="$1"
 HEIGHT="$2"
-
-# [TODO]: figure out how to avoid hard-coding this; (`tput cols` always returns 80)
-WIDTH=80
+WIDTH="$4"
 
 case $(file --brief --mime-type "$FILE") in
 image/*)
 	chafa --fill=block --symbols=block --colors=256 --size="$WIDTH"x"$HEIGHT" "$FILE"
 	;;
-text/* | application/json)
+text/troff)
+	man ./ "$1" | col -b
+	;;
+text/* | */xml | application/json)
 	# https://github.com/gokcehan/lf/issues/234#issuecomment-547592685
 	unset COLORTERM
-	bat --style=numbers,changes --wrap never --color always "$FILE"
+	bat --style=numbers,changes --terminal-width "$WIDTH" --wrap never --color always "$FILE"
 	;;
 application/zip)
 	unzip -l "$FILE"
