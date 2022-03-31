736c2a93f74583bd7e5f3802f329ed4d8fcbae5d
Ahmed El Gabri <ahmed@gabri.me>
2022-03-31 20:25:28 +0200

    vim: new quote

--- config/nvim/lua/_/config/startify.lua
+++ config/nvim/lua/_/config/startify.lua
@@ -155,6 +155,11 @@ return function()
         '',
         '— Milton Friedman',
       },
+      {
+        'The real problem is that programmers have spent far too much time worrying about efficiency in the wrong places and at the wrong times; premature optimization is the root of all evil (or at least most of it) in programming.',
+        '',
+        '— Donald Knuth',
+      },
     }
   )
 
