2d62dcfb9b43819f370fd83b89b22caf0c4feb87
Ahmed El Gabri <ahmed@gabri.me>
2022-05-21 23:13:46 +0200

    vim: update react snippets

--- config/nvim/plugin/snippets.lua
+++ config/nvim/plugin/snippets.lua
@@ -11,10 +11,170 @@ local i = ls.insert_node
 local f = ls.function_node
 local c = ls.choice_node
 local d = ls.dynamic_node
+local r = ls.restore_node
 local fmt = require('luasnip.extras.fmt').fmt
 local rep = require('luasnip.extras').rep
 -- https://github.com/L3MON4D3/LuaSnip/blob/1dbafec2379bd836bd09c4659d4c6e1a70eb380e/Examples/snippets.lua#L356=
 local l = require('luasnip.extras').lambda
+local types = require 'luasnip.util.types'
+
+ls.config.set_config {
+  history = true,
+  enable_autosnippets = true,
+  store_selection_keys = '<Tab>', -- needed for TM_SELECTED_TEXT
+  updateevents = 'TextChanged,TextChangedI', -- default is InsertLeave
+  ext_opts = {
+    [types.choiceNode] = {
+      active = {
+        virt_text = { { '← Choice', 'Todo' } },
+      },
+    },
+    -- [types.insertNode] = {
+    --   active = {
+    --     virt_text = { { '← ...', 'Todo' } },
+    --   },
+    -- },
+  },
+}
+
+require('luasnip.loaders.from_vscode').lazy_load {
+  path = {
+    '~/.config/nvim/pack/packer/start/friendly-snippets/snippets',
+    '~/.local-config/snippets',
+  },
+}
+-- Get a list of  the property names given an `interface_declaration`
+-- treesitter *tsx* node.
+-- Ie, if the treesitter node represents:
+--   interface {
+--     prop1: string;
+--     prop2: number;
+--   }
+-- Then this function would return `{"prop1", "prop2"}
+---@param id_node {} Stands for "interface declaration node"
+---@return string[]
+local function get_prop_names(id_node)
+  local object_type_node = id_node:child(2)
+  if object_type_node:type() ~= 'object_type' then
+    return {}
+  end
+
+  local prop_names = {}
+
+  for prop_signature in object_type_node:iter_children() do
+    if prop_signature:type() == 'property_signature' then
+      local prop_iden = prop_signature:child(0)
+      local prop_name = vim.treesitter.query.get_node_text(prop_iden, 0)
+      prop_names[#prop_names + 1] = prop_name
+    end
+  end
+
+  return prop_names
+end
+
+local react = {
+  s(
+    { trig = 'rcomp' },
+    fmt(
+      [[
+{}interface {}Props {{
+  {}
+}}
+
+{}function {}({{{}}}: {}Props) {{
+  return {}
+}}
+]],
+      {
+        i(1, 'export '),
+        -- Initialize component name to file name
+        d(2, function(_, snip)
+          return sn(nil, {
+            i(1, vim.fn.substitute(snip.env.TM_FILENAME, '\\..*$', '', 'g')),
+          })
+        end, { 1 }),
+        i(3, '// props'),
+        rep(1),
+        rep(2),
+        f(function(_, snip, _)
+          local pos_begin = snip.nodes[6].mark:pos_begin()
+          local pos_end = snip.nodes[6].mark:pos_end()
+          local parser = vim.treesitter.get_parser(0, 'tsx')
+          local tstree = parser:parse()
+
+          local node = tstree[1]:root():named_descendant_for_range(
+            pos_begin[1],
+            pos_begin[2],
+            pos_end[1],
+            pos_end[2]
+          )
+
+          while node ~= nil and node:type() ~= 'interface_declaration' do
+            node = node:parent()
+          end
+
+          if node == nil then
+            return ''
+          end
+
+          -- `node` is now surely of type "interface_declaration"
+          local prop_names = get_prop_names(node)
+
+          return table.concat(prop_names, ', ')
+        end, { 3 }),
+        rep(2),
+        i(5, "'Hello World!'"),
+      }
+    )
+  ),
+}
+
+local js_ts = {
+  s(
+    { trig = 'import', dscr = 'import statement' },
+    fmt("import {} from '{}{}';", {
+      i(1, 'name'),
+      i(2),
+      d(3, function(nodes)
+        local text = nodes[1][1]
+        local _, _, typish, target = text:find '^%s*(%a*)%s*{?%s*(%a+).*}?%s*$'
+        if typish == 'type' and target then
+          return sn(1, { i(1, target) })
+        elseif typish and target then
+          return sn(1, { i(1, typish .. target) })
+        else
+          return sn(1, { i(1, 'specifier') })
+        end
+      end, { 1 }),
+    })
+  ),
+  s(
+    { trig = 'require', dscr = 'require statement' },
+    fmt("const {} = require('{}{}');", {
+      i(1, 'name'),
+      i(2),
+      d(3, function(nodes)
+        local text = nodes[1][1]
+        return sn(1, { i(1, text) })
+      end, { 1 }),
+    })
+  ),
+  s({ trig = '**', dscr = 'docblock' }, {
+    t { '/**', '' },
+    f(function(_args, snip)
+      local lines = vim.tbl_map(function(line)
+        return ' * ' .. vim.trim(line)
+      end, snip.env.SELECT_RAW)
+      if #lines == 0 then
+        return ' * '
+      else
+        return lines
+      end
+    end, {}),
+    i(1),
+    t { '', ' */' },
+  }),
+}
 
 local function replace_each(replacer)
   return function(args)
@@ -152,7 +312,7 @@ ${0}]]
       t { '', '}' },
     }),
     ls.parser.parse_snippet(
-      { trig = 'foo' },
+      { trig = 'dumpenv' },
       [[
   ${$TM_SELECTED_TEXT} --  TM_SELECTED_TEXT The currently selected text or the empty string
   ${$TM_CURRENT_LINE} --  TM_CURRENT_LINE The contents of the current line
@@ -188,60 +348,10 @@ ${0}]]
     ]]
     ),
   },
-  javascript = {
-    -- Migrate this
-    -- {
-    --   "react boilerplate": {
-    --     "prefix": "react",
-    --     "body": [
-    --       "import * as React from 'react'",
-    --       "",
-    --       "interface ${TM_FILENAME_BASE/(.*)/${1:/capitalize}/}Props {\n\t\n}",
-    --       "",
-    --       "function ${TM_FILENAME_BASE/(.*)/${1:/capitalize}/}(${2:props}:${TM_FILENAME_BASE/(.*)/${1:/capitalize}/}Props) {\n\t$0\n}",
-    --       "",
-    --       "export default ${TM_FILENAME_BASE/(.*)/${1:/capitalize}/}"
-    --     ],
-    --     "description": "React boilerplate"
-    --   },
-    --   "react component": {
-    --     "prefix": "comp",
-    --     "body": [
-    --       "interface ${TM_FILENAME_BASE/(.*)/${1:/capitalize}/}Props {\n\t\n}",
-    --       "",
-    --       "function ${TM_FILENAME_BASE/(.*)/${1:/capitalize}/}(${2:props}:${TM_FILENAME_BASE/(.*)/${1:/capitalize}/}Props) {\n\t$0\n}"
-    --     ],
-    --     "description": "React component"
-    --   }
-    -- }
-    s(
-      { trig = 'require', dscr = 'require statement' },
-      fmt('const {variable} = require("{module}"){next}', {
-        variable = i(1, 'ModuleName'),
-        module = d(2, function(nodes)
-          return sn(1, { i(1, nodes[1][1]) })
-        end, {
-          1,
-        }),
-        next = i(0),
-      })
-    ),
-    s({ trig = '**', dscr = 'docblock' }, {
-      t { '/**', '' },
-      f(function(_, snip)
-        local lines = vim.tbl_map(function(line)
-          return ' * ' .. vim.trim(line)
-        end, snip.env.TM_SELECTED_TEXT)
-        if #lines == 0 then
-          return ' * '
-        else
-          return lines
-        end
-      end, {}),
-      i(1),
-      t { '', ' */' },
-    }),
-  },
+  javascript = js_ts,
+  typescript = js_ts,
+  javascriptreact = vim.tbl_extend('force', {}, js_ts, react),
+  typescriptreact = vim.tbl_extend('force', {}, js_ts, react),
   markdown = {
     ls.parser.parse_snippet(
       { trig = 'fmatter', dscr = 'Document frontmatter' },
@@ -362,17 +472,3 @@ SOFTWARE.
     }),
   },
 })
-
-ls.config.set_config {
-  history = true,
-  enable_autosnippets = true,
-  store_selection_keys = '<Tab>', -- needed for TM_SELECTED_TEXT
-  updateevents = 'TextChanged,TextChangedI', -- default is InsertLeave
-}
-
-require('luasnip.loaders.from_vscode').lazy_load {
-  path = {
-    '~/.config/nvim/pack/packer/start/friendly-snippets/snippets',
-    '~/.local-config/snippets',
-  },
-}
