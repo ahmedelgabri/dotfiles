3555c96f5a5986610de33a559348617bbaaaf991
Ahmed El Gabri <ahmed@gabri.me>
2022-08-02 21:22:00 +0200

    vim: detect new TypeScript extensions

---
+++ config/nvim/ftdetect/typescript.lua
@@ -0,0 +1,8 @@
+local au = require '_.utils.au'
+
+-- New extensions: https://www.typescriptlang.org/docs/handbook/esm-node.html#new-file-extensions
+au.autocmd {
+  event = { 'BufNewFile', 'BufRead' },
+  pattern = { '*.cts', '*.mts' },
+  command = 'noautocmd set filetype=typescript',
+}
