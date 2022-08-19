34b9186b3223234ca6f88edba8e50118d024294e
Ahmed El Gabri <ahmed@gabri.me>
2022-08-19 11:06:57 +0200

    vim: remove custom lua LSP binary logic

--- config/nvim/lua/_/config/lsp/sumneko.lua
+++ config/nvim/lua/_/config/lsp/sumneko.lua
@@ -1,29 +1,5 @@
-local cache_location = vim.fn.stdpath 'cache'
-local bin_folder = jit.os:lower() == 'osx' and 'macOS' or jit.os
-
-local nlua_nvim_lsp = {
-	base_directory = string.format(
-		'%s/nlua/sumneko_lua/lua-language-server/',
-		cache_location
-	),
-	bin_location = string.format(
-		'%s/nlua/sumneko_lua/lua-language-server/bin/%s/lua-language-server',
-		cache_location,
-		bin_folder
-	),
-}
-
-local sumneko_command = function()
-	return {
-		nlua_nvim_lsp.bin_location,
-		'-E',
-		string.format('%s/main.lua', nlua_nvim_lsp.base_directory),
-	}
-end
-
 return require('lua-dev').setup {
 	lspconfig = {
-		cmd = sumneko_command(),
 		settings = {
 			Lua = {
 				diagnostics = {
