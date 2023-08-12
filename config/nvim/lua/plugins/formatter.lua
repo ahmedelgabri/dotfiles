b39f58c1d29914af84ae1443aa6ffe613d9a79df
Ahmed El Gabri <ahmed@gabri.me>
2023-08-12 14:09:41 +0200

    vim: refactor formatter config

--- config/nvim/lua/plugins/formatter.lua
+++ config/nvim/lua/plugins/formatter.lua
@@ -22,118 +22,101 @@ local function prettier()
 			vim.fn.shellescape(vim.api.nvim_buf_get_name(0)),
 		},
 		stdin = true,
+		try_node_modules = true,
 	}
 end
 
 local function shfmt()
 	return {
 		exe = 'shfmt',
-		args = { '-' },
 		stdin = true,
 	}
 end
 
-local filetype = {
-	javascript = { prettier },
-	typescript = { prettier },
-	javascriptreact = { prettier },
-	typescriptreact = { prettier },
-	vue = { prettier },
-	['javascript.jsx'] = { prettier },
-	['typescript.tsx'] = { prettier },
-	['javascript.jest'] = { prettier },
-	['typescript.jest'] = { prettier },
-	markdown = { prettier },
-	['markdown.mdx'] = { prettier },
-	mdx = { prettier },
-	css = { prettier },
-	json = { prettier },
-	jsonc = { prettier },
-	scss = { prettier },
-	less = { prettier },
-	yaml = { prettier },
-	graphql = { prettier },
-	html = { prettier },
-	sh = { shfmt },
-	bash = { shfmt },
-	reason = {
-		function()
-			return {
-				exe = 'refmt',
-				stdin = true,
-			}
-		end,
-	},
-	rust = {
-		function()
-			return {
-				exe = 'rustfmt',
-				args = { '--emit=stdout' },
-				stdin = true,
-			}
-		end,
-	},
-	python = {
-		function()
-			return {
-				exe = 'ruff',
-				args = { '--fix', '-' },
-				stdin = true,
-			}
-		end,
-	},
-	go = {
-		function()
-			-- this will run gofmt too
-			return {
-				exe = 'goimports',
-				args = { '-w', vim.api.nvim_buf_get_name(0) },
-				stdin = false,
-			}
-		end,
-	},
-	nix = {
-		function()
-			return {
-				exe = 'nixpkgs-fmt',
-				stdin = true,
-			}
-		end,
-		function()
-			return {
-				exe = 'statix fix --stdin',
-				stdin = true,
-			}
-		end,
-	},
-	lua = {
-		function()
-			return {
-				exe = 'stylua',
-				args = {
-					'--line-endings',
-					'Unix',
-					'--quote-style',
-					'AutoPreferSingle',
-					'--call-parentheses',
-					'None',
-					'--indent-width',
-					vim.bo.tabstop,
-					'--column-width',
-					vim.bo.textwidth,
-					'-',
-				},
-				stdin = true,
-			}
-		end,
-	},
-}
-
 return {
 	'https://github.com/mhartington/formatter.nvim',
 	cmd = { 'FormatWrite' },
-	opts = {
-		logging = false,
-		filetype = filetype,
-	},
+	opts = function()
+		return {
+			logging = false,
+			filetype = {
+				javascript = { prettier },
+				typescript = { prettier },
+				javascriptreact = { prettier },
+				typescriptreact = { prettier },
+				vue = { prettier },
+				['javascript.jsx'] = { prettier },
+				['typescript.tsx'] = { prettier },
+				['javascript.jest'] = { prettier },
+				['typescript.jest'] = { prettier },
+				markdown = { prettier },
+				['markdown.mdx'] = { prettier },
+				mdx = { prettier },
+				css = { prettier },
+				json = { prettier },
+				jsonc = { prettier },
+				scss = { prettier },
+				less = { prettier },
+				yaml = { prettier },
+				graphql = { prettier },
+				html = { prettier },
+				sh = { shfmt },
+				bash = { shfmt },
+				reason = {
+					function()
+						return {
+							exe = 'refmt',
+							stdin = true,
+						}
+					end,
+				},
+				rust = {
+					require('formatter.filetypes.rust').rustfmt,
+				},
+				python = {
+					function()
+						return {
+							exe = 'ruff',
+							args = { '--fix', '-' },
+							stdin = true,
+						}
+					end,
+				},
+				go = {
+					-- this will run gofmt too
+					require('formatter.filetypes.go').goimports,
+				},
+				nix = {
+					require('formatter.filetypes.nix').nixpkgs_fmt,
+					function()
+						return {
+							exe = 'statix fix --stdin',
+							stdin = true,
+						}
+					end,
+				},
+				lua = {
+					function()
+						return {
+							exe = 'stylua',
+							args = {
+								'--line-endings',
+								'Unix',
+								'--quote-style',
+								'AutoPreferSingle',
+								'--call-parentheses',
+								'None',
+								'--indent-width',
+								vim.bo.tabstop,
+								'--column-width',
+								vim.bo.textwidth,
+								'-',
+							},
+							stdin = true,
+						}
+					end,
+				},
+			},
+		}
+	end,
 }
