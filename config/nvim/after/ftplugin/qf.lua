c442362b35f83d258e915acd04db9e4089a4802f
Ahmed El Gabri <ahmed@gabri.me>
2024-01-05 01:25:24 +0100

    vim: update quickfix settings

--- config/nvim/after/ftplugin/qf.lua
+++ config/nvim/after/ftplugin/qf.lua
@@ -1,3 +1,10 @@
 -- Wrap quickfix window
 vim.cmd [[setlocal wrap]]
 vim.cmd [[setlocal linebreak]]
+
+-- Some settings.
+vim.wo.nu = true
+vim.wo.rnu = true
+
+-- Add the cfilter plugin.
+vim.cmd.packadd 'cfilter'
