8a3204ff18bd5a95ca1f2ce7645836dd0132f63b
Ahmed El Gabri <ahmed@gabri.me>
2024-01-05 11:43:36 +0100

    vim: remove nvim-tree
    I mainly uses these file explorers to find the file I open in the tree,
    this is already doable with Netrw so no need for the plugin anymore.

--- config/nvim/after/plugin/netrw.lua
+++ config/nvim/after/plugin/netrw.lua
@@ -18,9 +18,28 @@ vim.g.netrw_bufsettings = 'noma nomod nonu nobl nowrap ro' -- default
 vim.g.netrw_bufsettings = vim.g.netrw_bufsettings
 	.. ' buftype=nofile bufhidden=wipe'
 
-vim.keymap.set(
-	{ 'n' },
-	'<leader>L',
-	':20Lex<CR>',
-	{ silent = true, desc = 'Open Netrw' }
-)
+-- :NvimTreeFindFile like functionality
+-- https://superuser.com/a/1814266
+vim.keymap.set('n', '--', function()
+	local relative_path = vim.fn.expand '%:h'
+	local startPos, endPos = string.find(relative_path, '/')
+
+	if startPos == 1 then
+		relative_path = '.'
+	end
+
+	vim.cmd [[:let @/=expand("%:t")]]
+	-- 20 is the size of the split
+	-- ! is to open it on the right side
+	vim.cmd('20Lexplore! ' .. relative_path)
+
+	if startPos > 1 then
+		while startPos ~= nil do
+			startPos, endPos = string.find(relative_path, '/', endPos + 1)
+			vim.fn['netrw#Call']('NetrwBrowseUpDir', 1)
+		end
+		vim.fn['netrw#Call']('NetrwBrowseUpDir', 1)
+	end
+
+	vim.cmd.normal 'n<CR>zz'
+end)
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -20,56 +20,6 @@ return {
 		end,
 	},
 	{
-		'https://github.com/kyazdani42/nvim-tree.lua',
-		keys = {
-			{
-				'--',
-				':NvimTreeFindFile<CR>',
-				{ silent = true },
-				desc = 'Open Nvimtree',
-			},
-		},
-		opts = {
-			view = {
-				side = 'right',
-			},
-			update_focused_file = {
-				enable = true,
-			},
-			git = {
-				ignore = false,
-			},
-			renderer = {
-				indent_markers = {
-					enable = false,
-				},
-				-- Normally README.md gets highlighted by default, which is a bit distracting.
-				special_files = {},
-				icons = {
-					show = {
-						git = true,
-						file = false,
-						folder = false,
-						folder_arrow = false,
-					},
-				},
-			},
-			actions = {
-				open_file = {
-					quit_on_open = true,
-					resize_window = true,
-					window_picker = {
-						enable = false,
-					},
-				},
-			},
-			-- vim-fugitive :GBrowse depends on netrw & this has to be set as early as possible
-			-- maybe switch to https://github.com/ruifm/gitlinker.nvim?
-			-- I only use fugitive for GBrowse 99% of the time & git branch in the statusline
-			disable_netrw = false,
-		},
-	},
-	{
 		'https://github.com/folke/which-key.nvim',
 		event = 'VeryLazy',
 		init = function()
