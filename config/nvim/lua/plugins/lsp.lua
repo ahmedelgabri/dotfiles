84bc08d4cf0a47a5f63ff5cbf11a0809e83185b2
Ahmed El Gabri <ahmed@gabri.me>
2024-02-05 01:47:26 +0100

    vim: disable warning on overriding a method

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -111,6 +111,8 @@ end
 -- globally override borders
 -- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#borders
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+
+---@diagnostic disable-next-line: duplicate-set-field
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
 	opts = opts or {}
 	opts.border = opts.border or getBorder()
