debfb560f4f4b4361dfe7c2307158c158f43a505
Ahmed El Gabri <ahmed@gabri.me>
2024-04-07 16:16:26 +0200

    vim: update ftplugin for JS/TS

--- config/nvim/after/ftplugin/javascript.lua
+++ config/nvim/after/ftplugin/javascript.lua
@@ -1,11 +1,13 @@
 vim.cmd [[setlocal conceallevel=2]]
 vim.cmd [[setlocal isfname+=@-@ ]]
 
-local package_lock =
-	vim.fn.findfile('package-lock.json', vim.fn.expand '%:p' .. ';')
+local yarn_lock = vim.fn.findfile('yarn.lock', vim.fn.expand '%:p' .. ';')
+local pnpm_lock = vim.fn.findfile('pnpm-lock.yaml', vim.fn.expand '%:p' .. ';')
 
-if vim.fn.filereadable(package_lock) == 1 then
-	vim.cmd [[setlocal makeprg=npm]]
-else
+if vim.fn.filereadable(yarn_lock) == 1 then
 	vim.cmd [[setlocal makeprg=yarn]]
+elseif vim.fn.filereadable(pnpm_lock) == 1 then
+	vim.cmd [[setlocal makeprg=pnpm]]
+else
+	vim.cmd [[setlocal makeprg=npm]]
 end
