6af47447ecd00bbf2b3f251fce76719c662d8234
Ahmed El Gabri <ahmed@gabri.me>
2024-05-20 13:15:43 +0200

    vim: use nvim_buf_get_extmarks to get all marks in a buffer after 0.10.0 and update statuscolumn

--- config/nvim/plugin/statuscolumn.lua
+++ config/nvim/plugin/statuscolumn.lua
@@ -1,36 +1,36 @@
 local M = {}
 _G.Status = M
 
----@return {name:string, text:string, texthl:string}[]
 function M.get_signs()
-	local buf = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
-	return vim.tbl_map(function(sign)
-		return vim.fn.sign_getdefined(sign.name)[1]
-	end, vim.fn.sign_getplaced(buf, { group = '*', lnum = vim.v.lnum })[1].signs)
-end
-
-local fcs = vim.opt.fillchars:get()
-function M.get_fold(lnum)
-	if vim.fn.foldlevel(lnum) <= vim.fn.foldlevel(lnum - 1) then
-		return ' '
-	end
-	return vim.fn.foldclosed(lnum) == -1 and fcs.foldopen or fcs.foldclose
-end
-
-function M.get_git_signs()
-	-- TODO: Check in 0.10 if we can get rid of M.get_signs and use this instead
-	-- https://github.com/echasnovski/mini.nvim/discussions/789#discussioncomment-9060348
-	local signs = vim.api.nvim_buf_get_extmarks(
+	return vim.api.nvim_buf_get_extmarks(
 		0,
 		-1,
 		{ vim.v.lnum - 1, 0 },
 		{ vim.v.lnum - 1, -1 },
 		{ type = 'sign', details = true }
 	)
+end
+
+local fcs = vim.opt.fillchars:get()
+function M.get_fold(lnum)
+	if vim.fn.foldlevel(lnum) <= vim.fn.foldlevel(lnum - 1) then
+		return '  '
+	end
+	return (vim.fn.foldclosed(lnum) == -1 and fcs.foldopen or fcs.foldclose)
+		.. ' '
+end
+
+function M.get_filtered_signs(signs, condition)
+	local cond = function(data)
+		if condition ~= nil then
+			return condition(data)
+		end
+		return true
+	end
 
 	for _, sign in ipairs(signs) do
 		local data = sign[4]
-		if data and data.sign_hl_group and data.sign_hl_group:find 'MiniDiff' then
+		if data and data.sign_hl_group and cond(data) then
 			return '%#' .. data.sign_hl_group .. '#' .. data.sign_text .. '%*'
 		end
 	end
@@ -39,21 +39,22 @@ function M.get_git_signs()
 end
 
 function M.column()
-	local sign
+	local signs = M.get_signs()
 
-	for _, s in ipairs(M.get_signs()) do
-		sign = s
-	end
-
-	local components = {
-		sign and ('%#' .. sign.texthl .. '#' .. sign.text .. '%*') or '  ',
-		[[%=]],
+	return table.concat({
+		-- Fold marker
 		M.get_fold(vim.v.lnum),
 		[[%=]],
-		M.get_git_signs(),
-	}
-
-	return table.concat(components, '')
+		-- Git signs
+		M.get_filtered_signs(signs, function(data)
+			return data.sign_hl_group:find 'MiniDiff'
+		end),
+		[[%=]],
+		-- Other signs
+		M.get_filtered_signs(signs, function(data)
+			return not data.sign_hl_group:find 'MiniDiff'
+		end),
+	}, '')
 end
 
 vim.opt.statuscolumn = [[%!v:lua.Status.column()]]
