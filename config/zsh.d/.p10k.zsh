b2f06d05516372288641a1ba4a0ee26eb864d5c6
Ahmed El Gabri <ahmed@gabri.me>
2024-06-14 22:22:19 +0200

    shell: p10k: give transient prompt a try

--- config/zsh.d/.p10k.zsh
+++ config/zsh.d/.p10k.zsh
@@ -176,7 +176,7 @@
   #   - always:   Trim down prompt when accepting a command line.
   #   - same-dir: Trim down prompt when accepting a command line unless this is the first command
   #               typed after changing current working directory.
-  typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=off
+  typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=always
 
   # Instant prompt mode.
   #
