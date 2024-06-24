8d48efecdee6d0033e1e8e803bdaafdf492737cf
Ahmed El Gabri <ahmed@gabri.me>
2024-06-24 12:23:27 +0200

    vim: change config to init for tmux-navigator plugin

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -8,7 +8,7 @@ return {
 	{
 		'https://github.com/christoomey/vim-tmux-navigator',
 		lazy = false,
-		config = function()
+		init = function()
 			vim.g.tmux_navigator_disable_when_zoomed = 1
 		end,
 	},
