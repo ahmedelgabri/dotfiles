698b42261d7c7c9346133f008fac1ce191530db8
Ahmed El Gabri <ahmed@gabri.me>
2024-08-19 01:56:28 +0200

    nix: update flakes

--- flake.nix
+++ flake.nix
@@ -131,7 +131,7 @@
           nixpkgs = {
             config = { allowUnfree = true; };
             overlays = [
-              (_: prev: {
+              (final: prev: {
                 pragmatapro = prev.callPackage ./nix/pkgs/pragmatapro.nix { };
                 hcron = prev.callPackage ./nix/pkgs/hcron.nix { };
 
@@ -142,6 +142,28 @@
                 notmuch = prev.notmuch.override {
                   withEmacs = false;
                 };
+
+                # TODO: remove when https://github.com/NixOS/nixpkgs/pull/335559 reaches nixpkgs-unstable
+                vscode-langservers-extracted = prev.vscode-langservers-extracted.overrideAttrs (_: {
+                  buildPhase =
+                    let
+                      extensions =
+                        if final.stdenv.isDarwin then
+                          "../VSCodium.app/Contents/Resources/app/extensions"
+                        else
+                          "../resources/app/extensions";
+                    in
+                    ''
+                      npx babel ${extensions}/css-language-features/server/dist/node \
+                        --out-dir lib/css-language-server/node/
+                      npx babel ${extensions}/html-language-features/server/dist/node \
+                        --out-dir lib/html-language-server/node/
+                      npx babel ${extensions}/json-language-features/server/dist/node \
+                        --out-dir lib/json-language-server/node/
+                      cp -r ${final.vscode-extensions.dbaeumer.vscode-eslint}/share/vscode/extensions/dbaeumer.vscode-eslint/server/out \
+                        lib/eslint-language-server
+                    '';
+                });
               })
             ];
           };
