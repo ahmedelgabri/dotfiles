7e3187a7d1165ef7be5874d43bb0edccaad47053
Ahmed El Gabri <ahmed@gabri.me>
2024-09-08 19:01:57 +0200

    vim: try venn.nvim plugin

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -47,5 +47,41 @@ return {
 	{ 'https://github.com/kevinhwang91/nvim-bqf', ft = 'qf' },
 	{ 'https://github.com/stevearc/quicker.nvim', ft = 'qf', config = true },
 	{ 'https://github.com/mistweaverco/kulala.nvim', ft = 'http', config = true },
+	{
+		'https://github.com/jbyuki/venn.nvim',
+		keys = {
+			{
+				'<leader>v',
+				function()
+					local venn_enabled = vim.inspect(vim.b.venn_enabled)
+					if venn_enabled == 'nil' then
+						vim.b.venn_enabled = true
+
+						vim.cmd [[setlocal ve=all]]
+
+						-- draw a line on HJKL keystokes
+						vim.keymap.set('n', 'J', '<C-v>j:VBox<CR>', { buffer = true })
+						vim.keymap.set('n', 'K', '<C-v>k:VBox<CR>', { buffer = true })
+						vim.keymap.set('n', 'L', '<C-v>l:VBox<CR>', { buffer = true })
+						vim.keymap.set('n', 'H', '<C-v>h:VBox<CR>', { buffer = true })
+						-- draw a box by pressing "f" with visual selection
+						vim.keymap.set('v', 'f', ':VBox<CR>', { buffer = true })
+					else
+						vim.cmd [[setlocal ve=]]
+
+						vim.keymap.del('n', 'J', { buffer = true })
+						vim.keymap.del('n', 'K', { buffer = true })
+						vim.keymap.del('n', 'L', { buffer = true })
+						vim.keymap.del('n', 'H', { buffer = true })
+						vim.keymap.del('v', 'f', { buffer = true })
+
+						vim.b.venn_enabled = nil
+					end
+				end,
+				noremap = true,
+				desc = 'Enable [V]enn diagramming mode',
+			},
+		},
+	},
 	{ 'https://github.com/pteroctopus/faster.nvim' },
 }
