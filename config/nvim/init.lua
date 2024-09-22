186423b473b4a598fd5a6c62c063c8e621466ed3
Ahmed El Gabri <ahmed@gabri.me>
2024-09-22 10:27:07 +0200

    vim: delete plugin_manager file and inline its contents

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -307,7 +307,46 @@ vim.g.markdown_fenced_languages = {
 	'viml=vim',
 }
 
-require '_.plugin_manager'
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system {
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
+		lazypath,
+	}
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup('plugins', {
+	dev = {
+		-- directory where you store your local plugin projects
+		path = '~/Sites/personal/forks',
+		---@type string[] plugins that match these patterns will use your local versions instead of being fetched from GitHub
+		patterns = { 'ahmedelgabri' }, -- For example {"folke"}
+		fallback = true, -- Fallback to git when local plugin doesn't exist
+	},
+	rtp = {
+		-- Stuff I don't use.
+		disabled_plugins = {
+			'getscript',
+			'getscriptPlugin',
+			'netrw',
+			'netrwPlugin',
+			'rplugin',
+			'rrhelper',
+			'tohtml',
+			'tutor',
+			'vimball',
+			'vimballPlugin',
+		},
+	},
+	-- Don't bother me when tweaking plugins.
+	change_detection = { notify = false },
+})
 
 -------------------------------------------------------------------------------
 -- OVERRIDES {{{1
