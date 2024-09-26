13724f4e09936b943cc060a1b4adaee07eceae65
Ahmed El Gabri <ahmed@gabri.me>
2024-09-26 23:01:38 +0200

    go: set up GOPRIVATE

--- nix/modules/shared/go.nix
+++ nix/modules/shared/go.nix
@@ -19,6 +19,7 @@ in
       my.env = rec {
         GOPATH = "$XDG_DATA_HOME/go";
         GOBIN = "${GOPATH}/bin";
+        GOPRIVATE = "github.com/${config.my.github_username}/*,gitlab.com/${config.my.github_username}/*";
       };
 
       # all tools from https://github.com/golang/vscode-go/blob/ed92a0c250e8941abb9adab973c129a263ba1e41/src/goToolsInformation.ts
