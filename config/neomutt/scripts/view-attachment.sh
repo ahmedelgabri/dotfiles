e94a8370213ff8987ef2b646c5e99690474d6150
Ahmed El Gabri <ahmed@gabri.me>
2024-09-26 23:03:35 +0200

    mail: fix shfmt errors and warnings

--- config/neomutt/scripts/view-attachment.sh
+++ config/neomutt/scripts/view-attachment.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-#
+#!/usr/bin/env bash
+
 # Author:  Eric Gebhart
 #
 # Purpose:  To be called by mutt as indicated by .mailcap to handle mail attachments.
@@ -70,36 +70,36 @@ type=$2
 open_with=$3
 
 # make sure the tmpdir exists.
-mkdir -p $tmpdir
+mkdir -p "$tmpdir"
 
 # clean it out.  Remove this if you want the directory
 # to accumulate attachment files.
-rm -f $tmpdir/*
+rm -f "$tmpdir/*"
 
 # Mutt puts everything in /tmp by default.
 # This gets the basic filename from the full pathname.
-filename=$(basename $1)
+filename=$(basename "$1")
 
 # get rid of the extension and save the name for later.
-file=$(echo $filename | cut -d"." -f1)
+file=$(echo "$filename" | cut -d"." -f1)
 
 if [ $debug = "yes" ]; then
-	echo "1:" $1 " 2:" $2 " 3:" $3 >$debug_file
-	echo "Filename:"$filename >>$debug_file
-	echo "File:"$file >>$debug_file
-	echo "===========================" >>$debug_file
+	echo "1: $1 2: $2 3: $3" >"$debug_file"
+	echo "Filename: $filename" >>"$debug_file"
+	echo "File: $file" >>"$debug_file"
+	echo "===========================" >>"$debug_file"
 fi
 
 # if the type is empty then try to figure it out.
-if [ -z $type ]; then
-	file $1
-	type=$(file -bi $1 | cut -d"/" -f2)
+if [ -z "$type" ]; then
+	file "$1"
+	type=$(file -bi "$1" | cut -d"/" -f2)
 fi
 
 # if the type is '-' then we don't want to mess with type.
 # Otherwise we are rebuilding the name.  Either from the
 # type that was passed in or from the type we discerned.
-if [ $type = "-" ]; then
+if [ "$type" = "-" ]; then
 	newfile=$filename
 else
 	newfile=$file.$type
@@ -109,19 +109,19 @@ newfile=$tmpdir/$newfile
 
 # Copy the file to our new spot so mutt can't delete it
 # before the app has a chance to view it.
-cp $1 $newfile
+cp "$1" "$newfile"
 
 if [ $debug = "yes" ]; then
-	echo "File:" $file "TYPE:" $type >>$debug_file
-	echo "Newfile:" $newfile >>$debug_file
-	echo "Open With:" $open_with >>$debug_file
+	echo "File: $file TYPE: $type" >>"$debug_file"
+	echo "Newfile: $newfile" >>"$debug_file"
+	echo "Open With: $open_with" >>"$debug_file"
 fi
 
 # If there's no 'open with' then we can let preview do it's thing.
 # Otherwise we've been told what to use.  So do an open -a.
 
-if [ -z $open_with ]; then
-	open $newfile
+if [ -z "$open_with" ]; then
+	open "$newfile"
 else
-	open -a "$open_with" $newfile
+	open -a "$open_with" "$newfile"
 fi
