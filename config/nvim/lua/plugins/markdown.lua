a13fecf0946d48cf0a520bf4f26f456f89c6854e
Ahmed El Gabri <ahmed@gabri.me>
2024-10-07 21:51:53 +0200

    vim: update markdown.nvim config

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -21,7 +21,48 @@ return {
 		'https://github.com/MeanderingProgrammer/markdown.nvim',
 		ft = { 'markdown' },
 		config = function()
-			require('render-markdown').setup {}
+			require('render-markdown').setup {
+				render_modes = { 'n', 'c', 'i', 'v', 'V', '\22' },
+				-- anti_conceal = { enabled = false },
+				sign = { enabled = false },
+				indent = { enabled = false },
+				dash = { icon = '━' },
+				heading = { position = 'inline' },
+				code = {
+					position = 'right',
+					width = 'block',
+					min_width = 45,
+					border = 'thick',
+				},
+				bullet = { right_pad = 1 },
+				link = {
+					custom = {
+						github = {
+							pattern = '^http[s]?://w*%.?github%.com/.*',
+							icon = require('mini.icons').get('directory', '.github') .. ' ',
+							highlight = 'RenderMarkdownGithubLink',
+						},
+						file = {
+							pattern = '^file:',
+							icon = "'",
+							highlight = 'RenderMarkdownFileLink',
+						},
+						youtube = {
+							pattern = '^http[s]?://www%.youtube%.com/.*',
+							icon = "'",
+							highlight = 'RenderMarkdownYoutubeLink',
+						},
+					},
+				},
+				pipe_table = {
+					preset = 'heavy',
+					cell = 'trimmed',
+				},
+				checkbox = {
+					-- position = 'overlay',
+					checked = { scope_highlight = '@markup.strikethrough' },
+				},
+			}
 		end,
 	},
 }
