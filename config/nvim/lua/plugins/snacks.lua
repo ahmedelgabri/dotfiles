49967c6596801d2ea63f17783828f12a5b06ed78
Ahmed El Gabri <ahmed@gabri.me>
2024-12-04 21:32:05 +0100

    vim: add snacks scratch mappings

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -135,5 +135,13 @@ return {
 				},
 			},
 		}
+
+		vim.keymap.set({ 'n' }, '<leader>.', function()
+			Snacks.scratch()
+		end, { desc = 'Toggle Scratch Buffer' })
+
+		vim.keymap.set({ 'n' }, '<leader>S', function()
+			Snacks.scratch.select()
+		end, { desc = 'Select Scratch Buffer' })
 	end,
 }
