0e4882d21d855a0e515be54be80138f03feca761
Ahmed El Gabri <ahmed@gabri.me>
2024-12-04 21:32:00 +0100

    vim: use default mini.surround mappings

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -120,41 +120,13 @@ return {
 	{
 		'https://github.com/echasnovski/mini.surround',
 		event = utils.LazyFile,
-		config = function()
-			require('mini.surround').setup {
-				mappings = {
-					add = 'ys',
-					delete = 'ds',
-					find = '',
-					find_left = '',
-					highlight = '',
-					replace = 'cs',
-					update_n_lines = '',
-
-					-- Add this only if you don't want to use extended mappings
-					suffix_last = '',
-					suffix_next = '',
-				},
-				search_method = 'cover_or_next',
-			}
-
-			-- Remap adding surrounding to Visual mode selection
-			vim.keymap.del('x', 'ys')
-			vim.keymap.set(
-				'x',
-				'S',
-				[[:<C-u>lua MiniSurround.add('visual')<CR>]],
-				{ silent = true, desc = '[S]urround in visual mode' }
-			)
-
-			-- Make special mapping for "add surrounding for line"
-			vim.keymap.set(
-				'n',
-				'yss',
-				'ys_',
-				{ remap = true, desc = 'Add surrounding for line' }
-			)
-		end,
+		opts = {
+			-- How to search for surrounding (first inside current line, then inside
+			-- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+			-- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
+			-- see `:h MiniSurround.config`.
+			search_method = 'cover_or_next',
+		},
 	},
 	{
 		'https://github.com/echasnovski/mini.trailspace',
