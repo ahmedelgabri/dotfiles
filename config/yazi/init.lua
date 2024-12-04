f4555ae077e86149ca8a3159f6570023d06f3c8c
Ahmed El Gabri <ahmed@gabri.me>
2024-12-04 21:31:45 +0100

    yazi: add extra space between icon and text

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -32,3 +32,14 @@ function Status:owner()
 		ui.Span ' ',
 	}
 end
+
+function Entity:icon()
+	local icon = self._file:icon()
+	if not icon then
+		return ui.Line ''
+	elseif self._file:is_hovered() then
+		return ui.Line(icon.text .. '  ')
+	else
+		return ui.Line(icon.text .. '  '):style(icon.style)
+	end
+end
