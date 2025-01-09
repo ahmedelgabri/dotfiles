9666a392c502f38dfe9b791a9d5006a2f2ef9d3d
Ahmed El Gabri <ahmed@gabri.me>
2025-01-09 22:19:55 +0100

    nix: update node template and move to volta

--- templates/node/flake.nix
+++ templates/node/flake.nix
@@ -22,12 +22,28 @@
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            fnm
+            volta
             nodePackages.pnpm
           ];
           shellHook = ''
-            eval "$(${pkgs.fnm}/bin/fnm env)"
-            ${pkgs.fnm}/bin/fnm use --install-if-missing --version-file-strategy=recursive
+            export VOLTA_HOME="$HOME/.volta"
+
+            # These are for direnv
+            export NODE_VERSIONS="$VOLTA_HOME/tools/image/node/"
+            export NODE_VERSION_PREFIX=""
+
+            export __DIRENV_NODE_VERSION__
+
+            if [ -f "./.nvmrc" ]; then
+               __DIRENV_NODE_VERSION__="$(<./.nvmrc)"
+            elif [ -f "./.node-version" ]; then
+               __DIRENV_NODE_VERSION__="$(<./.node-version)"
+            fi
+
+            # We only install it, direnv will load it
+            if [ -x "$VOLTA_HOME/bin/volta" ]; then
+            	volta install node@"$__DIRENV_NODE_VERSION__"
+            fi
           '';
         };
       }
