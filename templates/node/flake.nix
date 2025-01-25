68bdf97da68ba0f5798c6412b0e1ace80bb82781
Ahmed El Gabri <ahmed@gabri.me>
2025-01-25 13:34:59 +0100

    nix: handle node versioning differently

--- templates/node/flake.nix
+++ templates/node/flake.nix
@@ -19,27 +19,6 @@
           inherit system;
           config.allowUnfree = true;
         };
-
-        # Grab node version from .nvmrc file if exits otherwise fallback to latest node
-        # version schema depends on volta version schema
-        getNodeVersion = list: let
-          result =
-            builtins.filter (v: v != "")
-            (builtins.map (
-                p:
-                  if builtins.pathExists p
-                  then nixpkgs.lib.trim (builtins.readFile p)
-                  else ""
-              )
-              list);
-        in
-          if builtins.length result > 0
-          then "node@${builtins.head result}"
-          else "node";
-
-        # This is a list of paths not strings, order matters because it will
-        # return the first version it finds
-        nodeVersion = getNodeVersion [./.nvmrc ./.node-version];
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
@@ -47,23 +26,6 @@
             nodePackages.pnpm
           ];
           shellHook = ''
-            export VOLTA_HOME="$HOME/.volta"
-
-            # These are for direnv
-            export NODE_VERSIONS="$VOLTA_HOME/tools/image/node/"
-            export NODE_VERSION_PREFIX=""
-
-            # direnv wants only the version number, empty string will let it get the latest
-            export __DIRENV_NODE_VERSION__="${
-              if nodeVersion == "node"
-              then ""
-              else nodeVersion
-            }"
-
-            # We only install it, direnv will load it
-            if [ -x "$VOLTA_HOME/bin/volta" ]; then
-            	volta install "${nodeVersion}"
-            fi
           '';
         };
       }
