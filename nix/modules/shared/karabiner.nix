cdfa28cc24cdea1bcc2526cb4fa333b23b65fd81
Ahmed El Gabri <ahmed@gabri.me>
2025-02-09 21:41:38 +0100

    karabiner: symlink config instead of linking from the nix store

--- nix/modules/shared/karabiner.nix
+++ nix/modules/shared/karabiner.nix
@@ -3,6 +3,8 @@
   config,
   ...
 }: let
+  inherit (config.my.user) home;
+  inherit (config.my) hm;
   cfg = config.my.modules.karabiner;
 in {
   options = with lib; {
@@ -19,10 +21,26 @@ in {
         "karabiner-elements"
       ];
 
-      my.hm.file = {
-        ".config/karabiner/karabiner.json" = {
-          source = ../../../config/karabiner/karabiner.json;
-        };
-      };
+      # my.hm.file = {
+      #   ".config/karabiner/karabiner.json" = {
+      #     source = ../../../config/karabiner/karabiner.json;
+      #   };
+      # };
+
+      # The config should be "live" because it can be modified from the app GUI
+      # At least for now and until I reach a stable state, I'd like to symlink it instead
+      system.activationScripts.postUserActivation.text =
+        /*
+        bash
+        */
+        ''
+          echo ":: -> Running karabiner activationScript..."
+
+          # Handle mutable configs
+          if [ ! -e "${hm.configHome}/karabiner/" ]; then
+            echo "Linking karabiner folders..."
+            ln -sf ${home}/.dotfiles/config/karabiner ${hm.configHome}
+          fi
+        '';
     };
 }
