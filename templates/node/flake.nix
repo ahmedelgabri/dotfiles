d0e7535add171efabaed9065d456c815745c7952
Ahmed El Gabri <ahmed@gabri.me>
2025-02-17 20:56:01 +0100

    nix: update node installation in node template

--- templates/node/flake.nix
+++ templates/node/flake.nix
@@ -25,8 +25,29 @@
             volta
             nodePackages.pnpm
           ];
-          shellHook = ''
-          '';
+          shellHook =
+            /*
+            bash
+            */
+            ''
+              if [[ ! -d "$HOME/.volta" ]]; then
+                mkdir -p "$HOME/.volta"
+              fi
+
+              export VOLTA_HOME="$HOME/.volta";
+              export PATH="$VOLTA_HOME/bin:$PATH"
+              export NODE_VERSIONS="$VOLTA_HOME/tools/image/node"
+              export NODE_VERSION_PREFIX=""
+              export __DIRENV_NODE_VERSION__=""
+
+              if [ -f "./.nvmrc" ]; then
+                 __DIRENV_NODE_VERSION__="$(<./.nvmrc)"
+              elif [ -f "./.node-version" ]; then
+                 __DIRENV_NODE_VERSION__="$(<./.node-version)"
+              fi
+
+              volta install node@"$__DIRENV_NODE_VERSION__"
+            '';
         };
       }
     );
