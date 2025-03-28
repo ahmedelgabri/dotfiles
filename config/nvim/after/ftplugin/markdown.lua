013f526a3c918e6fe15ae5ca23b126dbd9f34f1c
Ahmed El Gabri <ahmed@gabri.me>
2025-03-28 22:08:22 +0100

    refactor(neovim): remove treesitter check for folding in markdown
    The check for nvim-treesitter before setting folding options is redundant as Treesitter is assumed to be installed.

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -3,10 +3,7 @@ local utils = require '_.utils'
 vim.opt.conceallevel = 2
 vim.opt.concealcursor = 'c'
 
-local has_treesitter = pcall(require, 'nvim-treesitter')
-if has_treesitter then
-	vim.opt_local.foldmethod = 'expr'
-	vim.opt_local.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-end
+vim.opt_local.foldmethod = 'expr'
+vim.opt_local.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 
 utils.plaintext()
