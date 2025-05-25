e99d8dbfa14617660f610aaaeac1797d0ab187b7
Ahmed El Gabri <ahmed@gabri.me>
2025-05-25 19:47:13 +0200

    refactor(neovim): change treesitter config to track main
    - master was deprecated and main will have the new changes
    - Remove utils dependency and switch to eager loading
    - Add file size limit (1MB) for rainbow delimiters
    - Add language whitelist for rainbow delimiters
    - Fix treesitter setup configuration
    - Update to use main branch

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,7 @@
-local utils = require '_.utils'
-
 return {
 	'https://github.com/nvim-treesitter/nvim-treesitter',
-	event = utils.LazyFile,
+	branch = 'main',
+	lazy = false,
 	build = ':TSUpdate',
 	dependencies = {
 		{
@@ -25,6 +24,22 @@ return {
 			'https://github.com/HiPhish/rainbow-delimiters.nvim',
 			config = function()
 				require('rainbow-delimiters.setup').setup {
+					enabled_when = function(bufnr)
+						local max_filesize = 1 * 1024 * 1024 -- 1 MB
+						local ok, stats =
+							pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+						if ok and stats and stats.size > max_filesize then
+							return false
+						end
+						return true
+					end,
+					whitelist = {
+						'html',
+						'javascript',
+						'jsx',
+						'typescript',
+						'tsx',
+					},
 					query = {
 						javascript = 'rainbow-tags-react',
 						typescript = 'rainbow-tags-react',
@@ -63,7 +78,7 @@ return {
 
 		-- See https://github.com/andreaswachowski/dotfiles/commit/853fbc1e06595ecd18490cdfad64823be8bb9971
 		--- @diagnostic disable-next-line: missing-fields
-		require('nvim-treesitter.configs').setup {
+		require('nvim-treesitter').setup {
 			sync_install = false,
 			auto_install = true,
 			ensure_installed = {
@@ -108,7 +123,6 @@ return {
 				enable = true,
 				use_languagetree = true,
 				additional_vim_regex_highlighting = { 'markdown' }, -- for the obsidian style %% comments
-			},
 			},
 		}
 
