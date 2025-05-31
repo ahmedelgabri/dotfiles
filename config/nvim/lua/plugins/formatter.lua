366c87305505fb1c58030acb5ac9cdc317b5605f
Ahmed El Gabri <ahmed@gabri.me>
2025-05-31 22:52:17 +0200

    feat(neovim): add commands to toggle autoformat-on-save
    Add FormatDisable and FormatEnable commands to control autoformat behavior:
    - FormatDisable  - disable autoformat globally
    - FormatDisable! - disable autoformat for current buffer only
    - FormatEnable   - re-enable autoformat

--- config/nvim/lua/plugins/formatter.lua
+++ config/nvim/lua/plugins/formatter.lua
@@ -151,6 +151,25 @@ return {
 					range = range,
 				}
 			end, { range = true })
+
+			vim.api.nvim_create_user_command('FormatDisable', function(args)
+				if args.bang then
+					-- FormatDisable! will disable formatting just for this buffer
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = 'Disable autoformat-on-save',
+				bang = true,
+			})
+
+			vim.api.nvim_create_user_command('FormatEnable', function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = 'Re-enable autoformat-on-save',
+			})
 		end,
 	},
 }
