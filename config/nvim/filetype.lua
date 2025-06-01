ed562f9679b28e932908e9d807b56a33bed11c94
Ahmed El Gabri <ahmed@gabri.me>
2025-06-01 15:15:18 +0200

    feat(neovim): improve filetype detection
    This commit enhances filetype detection by adding and refining filetype associations in `filetype.lua`.
    
    - Updated patterns for `jsonc`, `dotenv`, `groovy`, `yaml.github`, and `yaml.docker-compose` to improve accuracy.
    - Removed some unnecessary extensions.

--- config/nvim/filetype.lua
+++ config/nvim/filetype.lua
@@ -1,13 +1,7 @@
-if not vim.filetype then
-	return
-end
-
 vim.filetype.add {
 	extension = {
 		mdx = 'mdx',
-		http = 'http',
 		log = 'log',
-		conf = 'conf',
 		env = 'dotenv',
 		sb = 'scheme', -- Apple sandbox rules
 		['code-workspace'] = 'jsonc',
@@ -26,19 +20,17 @@ vim.filetype.add {
 		PULLREQ_EDITMSG = 'markdown.ghpull',
 		ISSUE_EDITMSG = 'markdown.ghissue',
 		RELEASE_EDITMSG = 'markdown.ghrelease',
-		-- For dockder compose-language-service
-		['docker-compose.yaml'] = 'yaml.docker-compose',
-		['docker-compose.yml'] = 'yaml.docker-compose',
-		['compose.yaml'] = 'yaml.docker-compose',
-		['compose.yml'] = 'yaml.docker-compose',
 	},
 	pattern = {
-		['tsconfig*.json'] = 'jsonc',
+		['[jt]sconfig*.json'] = 'jsonc',
 		['.*/%.vscode/.*%.json'] = 'jsonc',
 		-- INFO: Match filenames like - ".env.example", ".env.local" and so on
 		-- needed to make dotenv-linter with null-ls works correctly
 		['%.env%.[%w_.-]+'] = 'dotenv',
 		['.*%.gradle'] = 'groovy',
 		['.*/%.github/.*%.y*ml'] = 'yaml.github',
+		-- For dockder compose-language-service
+		['compose.y.?ml'] = 'yaml.docker-compose',
+		['docker%-compose%.y.?ml'] = 'yaml.docker-compose',
 	},
 }
