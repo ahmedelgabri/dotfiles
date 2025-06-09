ddb9b5cd745a00b57a44c9595bc10d141107e11f
Ahmed El Gabri <ahmed@gabri.me>
2025-06-09 14:42:16 +0200

    fix(neovim): set foldminlines to 1 for markdown
    Workaround for neovim/neovim#33926.

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -6,4 +6,7 @@ vim.wo.concealcursor = 'c'
 vim.wo.foldmethod = 'expr'
 vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 
+-- Workaround for: https://github.com/neovim/neovim/issues/33926
+vim.wo.foldminlines = 1
+
 utils.plaintext()
