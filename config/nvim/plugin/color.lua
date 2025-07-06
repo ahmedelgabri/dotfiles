b2d843b2fc753b789faadaf9c1fa38cec1a016e1
Ahmed El Gabri <ahmed@gabri.me>
2025-07-06 19:26:26 +0200

    feat(neovim): sync Neovim background with macOS appearance
    Automatically sets Neovim's `background` option to 'light' or 'dark' based on the current macOS system-wide appearance settings. This is triggered on `VimEnter` and `FocusGained` events.

--- config/nvim/plugin/color.lua
+++ config/nvim/plugin/color.lua
@@ -1,6 +1,30 @@
 local au = require '_.utils.au'
 
+local function set_macos_colorscheme()
+	if vim.loop.os_uname().sysname ~= 'Darwin' then
+		vim.o.background = 'dark'
+		print 'Not macOS, switch to Dark mode as default.'
+	end
+	local appleInterfaceStyle =
+		vim.fn.system { 'defaults', 'read', '-g', 'AppleInterfaceStyle' }
+
+	-- The not is because in light mode (which is the default) you get
+	-- "The domain/default pair of (kCFPreferencesAnyApplication, AppleInterfaceStyle) does not exist"
+	if not appleInterfaceStyle:find 'Dark' then
+		vim.o.background = 'light'
+		print 'Switched to Light Mode'
+	else
+		vim.o.background = 'dark'
+		print 'Switched to Dark Mode'
+	end
+end
+
 au.augroup('__MyCustomColors__', {
+	{
+		event = { 'VimEnter', 'FocusGained' },
+		callback = set_macos_colorscheme,
+		desc = 'Set colorscheme based on macOS appearance',
+	},
 	{
 		event = { 'UIEnter', 'ColorScheme' },
 		callback = function()
