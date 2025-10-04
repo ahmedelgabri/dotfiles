c68467f0aa5140a81c0dfe7aa5327e09f069b3d9
Ahmed El Gabri <ahmed@gabri.me>
2025-10-04 21:55:36 +0200

    refactor(nix): reorganize devShells and extract Go tooling
    - Move general tools (typos, alejandra) to default devShell
    - Create separate 'go' devShell for Go-specific development tools
    - Remove commented-out shellHook

--- flake.nix
+++ flake.nix
@@ -290,16 +290,20 @@
       default = pkgs.mkShell {
         name = "dotfiles";
         packages = with pkgs; [
+          typos
+          typos-lsp
+          alejandra
+        ];
+      };
+      go = pkgs.mkShell {
+        name = "dotfiles-go";
+        packages = with pkgs; [
           go
           gopls
           go-tools # staticcheck, etc...
           gomodifytags
           gotools # goimports
-          typos
-          typos-lsp
-          alejandra
         ];
-        # shellHook = ''echo "hi"'';
       };
     });
   in
