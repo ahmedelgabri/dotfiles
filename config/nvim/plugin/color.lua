f7372f542ddb01b19d5bb4677d526d20f12e48f9
Ahmed El Gabri <ahmed@gabri.me>
2025-10-25 21:53:26 +0200

    refactor(neovim): migrate terminal background sync to mini.misc
    Replace custom terminal background synchronization autocommands with
    mini.misc.setup_termbg_sync() for cleaner implementation.

--- config/nvim/plugin/color.lua
+++ config/nvim/plugin/color.lua
@@ -25,33 +25,6 @@ au.augroup('__MyCustomColors__', {
 		callback = set_macos_colorscheme,
 		desc = 'Set colorscheme based on macOS appearance',
 	},
-	{
-		event = { 'UIEnter', 'ColorScheme' },
-		callback = function()
-			local normal = vim.api.nvim_get_hl(0, { name = 'Normal' })
-			if not normal.bg then
-				return
-			end
-
-			if vim.env.TMUX then
-				io.write(
-					string.format('\027Ptmux;\027\027]11;#%06x\007\027\\', normal.bg)
-				)
-			else
-				io.write(string.format('\027]11;#%06x\027\\', normal.bg))
-			end
-		end,
-	},
-	{
-		event = 'UILeave',
-		callback = function()
-			if vim.env.TMUX then
-				io.write '\027Ptmux;\027\027]111;\007\027\\'
-			else
-				io.write '\027]111\027\\'
-			end
-		end,
-	},
 })
 
 vim.cmd.colorscheme 'plain'
