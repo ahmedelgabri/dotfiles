50b446b0516490aaf2bbd6eddc0b36edc5ee1a0f
Ahmed El Gabri <ahmed@gabri.me>
2025-11-02 19:50:00 +0100

    feat(neovim): add leetcode plugin keybindings
    Add comprehensive keybindings for leetcode.nvim plugin operations including
    console, run, test, submit, and list commands. Refactor configuration to use
    explicit config function instead of opts function for proper keymap setup.
    
    - Add <localleader>lc for console
    - Add <localleader>lr for run
    - Add <localleader>lt for test
    - Add <localleader>ls for submit
    - Add <localleader>ll for listing all questions
    - Add <localleader>lL for listing in-progress questions
    - Simplify opts table structure

--- config/nvim/lua/plugins/leetcode.lua
+++ config/nvim/lua/plugins/leetcode.lua
@@ -1,16 +1,52 @@
+local leet_arg = 'leet'
+
 return {
 	{
 		'https://github.com/kawre/leetcode.nvim',
+		lazy = leet_arg ~= vim.fn.argv()[1],
 		cmd = 'Leet',
 		dependencies = {
 			'https://github.com/ibhagwan/fzf-lua',
 			'https://github.com/nvim-lua/plenary.nvim',
 			'https://github.com/MunifTanjim/nui.nvim',
 		},
-		opts = function(_, opts)
-			return vim.tbl_deep_extend('force', opts or {}, {
+		config = function()
+			local map = vim.api.nvim_set_keymap
+
+			map(
+				'n',
+				'<localleader>lc',
+				'<Cmd>Leet console<Cr>',
+				{ desc = 'Leet: Console' }
+			)
+			map('n', '<localleader>lr', '<Cmd>Leet run<Cr>', { desc = 'Leet: Run' })
+			map(
+				'n',
+				'<localleader>ls',
+				'<Cmd>Leet submit<Cr>',
+				{ desc = 'Leet: Submit' }
+			)
+			map(
+				'n',
+				'<localleader>ll',
+				'<Cmd>Leet list<Cr>',
+				{ desc = 'Leet: Select question (all)' }
+			)
+			map(
+				'n',
+				'<localleader>lL',
+				'<Cmd>Leet list status=notac<Cr>',
+				{ desc = 'Leet: Select question (in progress)' }
+			)
+
+			require('leetcode').setup {
+				arg = leet_arg,
 				lang = 'typescript',
-			})
+				storage = {
+					home = (vim.env.PROJECTS or vim.fn.stdpath 'data') .. '/leetcode',
+					cache = vim.fn.stdpath 'cache' .. '/leetcode',
+				},
+			}
 		end,
 	},
 }
