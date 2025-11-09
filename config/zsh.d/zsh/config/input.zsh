55ed08e301ab322508ef471fc74f6b2f6eb7cb6c
Ahmed El Gabri <ahmed@gabri.me>
2025-11-09 11:03:34 +0100

    style: Run `nix fmt`

--- config/zsh.d/zsh/config/input.zsh
+++ config/zsh.d/zsh/config/input.zsh
@@ -3,7 +3,7 @@
 #
 
 if [ -t 0 ]; then
-	stty -ixon # allow C-s and C-q to be used for things
+  stty -ixon # allow C-s and C-q to be used for things
 fi
 
 bindkey -v
@@ -12,49 +12,49 @@ bindkey -v
 zmodload -F zsh/terminfo +b:echoti +p:terminfo
 typeset -gA key_info
 key_info=(
-	'Control' '\C-'
-	'ControlLeft' '\e[1;5D \e[5D \e\e[D \eOd \eOD'
-	'ControlRight' '\e[1;5C \e[5C \e\e[C \eOc \eOC'
-	'Escape' '\e'
-	'Meta' '\M-'
-	'Backspace' ${terminfo[kbs]}
-	'BackTab' ${terminfo[kcbt]}
-	'Left' ${terminfo[kcub1]}
-	'Down' ${terminfo[kcud1]}
-	'Right' ${terminfo[kcuf1]}
-	'Up' ${terminfo[kcuu1]}
-	'Delete' ${terminfo[kdch1]}
-	'End' ${terminfo[kend]}
-	'F1' ${terminfo[kf1]}
-	'F2' ${terminfo[kf2]}
-	'F3' ${terminfo[kf3]}
-	'F4' ${terminfo[kf4]}
-	'F5' ${terminfo[kf5]}
-	'F6' ${terminfo[kf6]}
-	'F7' ${terminfo[kf7]}
-	'F8' ${terminfo[kf8]}
-	'F9' ${terminfo[kf9]}
-	'F10' ${terminfo[kf10]}
-	'F11' ${terminfo[kf11]}
-	'F12' ${terminfo[kf12]}
-	'Home' ${terminfo[khome]}
-	'Insert' ${terminfo[kich1]}
-	'PageDown' ${terminfo[knp]}
-	'PageUp' ${terminfo[kpp]}
+  'Control' '\C-'
+  'ControlLeft' '\e[1;5D \e[5D \e\e[D \eOd \eOD'
+  'ControlRight' '\e[1;5C \e[5C \e\e[C \eOc \eOC'
+  'Escape' '\e'
+  'Meta' '\M-'
+  'Backspace' ${terminfo[kbs]}
+  'BackTab' ${terminfo[kcbt]}
+  'Left' ${terminfo[kcub1]}
+  'Down' ${terminfo[kcud1]}
+  'Right' ${terminfo[kcuf1]}
+  'Up' ${terminfo[kcuu1]}
+  'Delete' ${terminfo[kdch1]}
+  'End' ${terminfo[kend]}
+  'F1' ${terminfo[kf1]}
+  'F2' ${terminfo[kf2]}
+  'F3' ${terminfo[kf3]}
+  'F4' ${terminfo[kf4]}
+  'F5' ${terminfo[kf5]}
+  'F6' ${terminfo[kf6]}
+  'F7' ${terminfo[kf7]}
+  'F8' ${terminfo[kf8]}
+  'F9' ${terminfo[kf9]}
+  'F10' ${terminfo[kf10]}
+  'F11' ${terminfo[kf11]}
+  'F12' ${terminfo[kf12]}
+  'Home' ${terminfo[khome]}
+  'Insert' ${terminfo[kich1]}
+  'PageDown' ${terminfo[knp]}
+  'PageUp' ${terminfo[kpp]}
 )
 
 # Bind the keys
 
 if [[ ${zdouble_dot_expand} == 'true' ]]; then
-	double-dot-expand() {
-		if [[ ${LBUFFER} == *.. ]]; then
-			LBUFFER+='/..'
-		else
-			LBUFFER+='.'
-		fi
-	}
-	zle -N double-dot-expand
-	bindkey '.' double-dot-expand
+  double-dot-expand() {
+    if [[ ${LBUFFER} == *.. ]]; then
+      LBUFFER+='/..'
+    else
+      LBUFFER+='.'
+    fi
+  }
+  zle -N double-dot-expand
+  bindkey '.' double-dot-expand
 fi
 
 # Expandpace.
@@ -67,14 +67,14 @@ bindkey "${key_info[Control]}L" clear-screen
 [[ -n ${key_info[BackTab]} ]] && bindkey ${key_info[BackTab]} reverse-menu-complete
 
 autoload -Uz is-at-least && if ! is-at-least 5.3; then
-	# Redisplay after completing, and avoid blank prompt after <Tab><Tab><Ctrl-C>
-	expand-or-complete-with-redisplay() {
-		print -Pn '...'
-		zle expand-or-complete
-		zle redisplay
-	}
-	zle -N expand-or-complete-with-redisplay
-	bindkey "${key_info[Control]}I" expand-or-complete-with-redisplay
+  # Redisplay after completing, and avoid blank prompt after <Tab><Tab><Ctrl-C>
+  expand-or-complete-with-redisplay() {
+    print -Pn '...'
+    zle expand-or-complete
+    zle redisplay
+  }
+  zle -N expand-or-complete-with-redisplay
+  bindkey "${key_info[Control]}I" expand-or-complete-with-redisplay
 fi
 
 autoload -Uz edit-command-line
