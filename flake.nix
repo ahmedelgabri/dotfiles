b6954ff8ab81111ba5838d892447c99f5e6b70ad
Ahmed El Gabri <ahmed@gabri.me>
2026-03-07 10:29:56 +0100

    chore: drop x86_64-darwin support

--- flake.nix
+++ flake.nix
@@ -76,7 +76,6 @@
 
   outputs = {self, ...} @ inputs: let
     darwinHosts = {
-      "pandoras-box" = "x86_64-darwin";
       "alcantara" = "aarch64-darwin";
       "rocket" = "aarch64-darwin";
     };
--- templates/bun/flake.nix
+++ templates/bun/flake.nix
@@ -10,7 +10,7 @@
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
       perSystem = {
         pkgs,
         system,
